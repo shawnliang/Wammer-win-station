c7d3cc472c4b3344e44554e2c9965fc4432aa929
Shawn Liang <shawn.liang@waveface.com>
2011-10-07 17:20:24 +0800

    add a class to signin user

---
+++ UT_WammerStation/TestUserSingOn.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net;
+using System.IO;
+
+namespace UT_WammerStation
+{
+    [TestClass]
+    public class TestUserSingOn
+    {
+        string AbsPath;
+
+        private void connected(IAsyncResult result)
+        {
+            System.Net.HttpListener listener = (System.Net.HttpListener)result.AsyncState;
+            HttpListenerContext context = listener.EndGetContext(result);
+            AbsPath = context.Request.Url.AbsolutePath;
+            context.Response.StatusCode = 200;
+
+            using (StreamWriter w = new StreamWriter(context.Response.OutputStream))
+            {
+                w.WriteLine("{ \"status\": 200, \"uid\": \"id1\", \"userToken\": \"token1\" }");
+            }
+        }
+
+        [TestMethod]
+        public void TestSignOn()
+        {
+            System.Net.HttpListener listener = new System.Net.HttpListener();
+            listener.Prefixes.Add("http://+:80/");
+
+            // If you get an Access Denied exception in Windows 7 or Windows 2008 or later, 
+            // you might need to reserve a namespace. Run a console window as Admin, and type something like
+            //    netsh http add urlacl http://+:80/ user=domain\user
+            // See this page: 
+            //    http://msdn.microsoft.com/en-us/library/cc307223(VS.85).aspx
+            listener.Start();
+            listener.BeginGetContext(this.connected, listener);
+
+            Wammer.Cloud.User user = Wammer.Cloud.User.SignIn("user1", "passwd1", "apiKey1");
+            Assert.AreEqual("user1", user.Name);
+            Assert.AreEqual("passwd1", user.Password);
+            Assert.AreEqual("/api/v2/auth/login/email/user1/password/passwd1/apiKey/apiKey1", AbsPath);
+            Assert.AreEqual("token1", user.Token);
+            Assert.AreEqual("id1", user.Id);
+        }
+    }
+}
---
+++ Wammer.Station/Cloud/User.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Web;
+
+namespace Wammer.Cloud
+{
+    public class UserSigninResponse
+    {
+        private int _status;
+        private string _userToken;
+        private string _uid;
+
+        public UserSigninResponse()
+        {
+        }
+
+        public string uid
+        {
+            get { return _uid; }
+            set { _uid = value; }
+        }
+
+        public int status
+        {
+            get { return _status; }
+            set { _status = value; }
+        }
+
+        public string userToken
+        {
+            get { return _userToken; }
+            set { _userToken = value; }
+        }
+    }
+
+    public class User
+    {
+        private string name;
+        private string password;
+        private string id;
+        private string token;
+
+
+        private User(string username, string passwd)
+        {
+            this.name = username;
+            this.password = passwd;
+        }
+
+        public static User SignIn(string username, string passwd, string apiKey)
+        {
+            WebClient http = new WebClient();
+            string address = string.Format("http://{0}:{1}/api/v2/auth/login/email/{2}/password/{3}/apiKey/{4}",
+                "127.0.0.1", 80, 
+                HttpUtility.UrlEncode(username), 
+                HttpUtility.UrlEncode(passwd), 
+                HttpUtility.UrlEncode(apiKey));
+
+            string response = http.DownloadString(address);
+            UserSigninResponse res = fastJSON.JSON.Instance.ToObject<UserSigninResponse>(response);
+
+            User user = new User(username, passwd);
+            user.id = res.uid;
+            user.token = res.userToken;
+            return user;
+        }
+
+        public string Name
+        {
+            get { return name; }
+        }
+
+        public string Password
+        {
+            get { return password; }
+        }
+
+        public string Id
+        {
+            get { return this.id; }
+        }
+
+        public string Token
+        {
+            get { return this.token; }
+        }
+    }
+}
