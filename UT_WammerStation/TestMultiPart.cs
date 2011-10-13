4133dedbd477cd787b39218f649fd04b1ee11a90
Shawn Liang <shawn.liang@waveface.com>
2011-10-13 16:17:06 +0800

    add missing checkins

--- UT_WammerStation/TestMultiPart.cs
+++ UT_WammerStation/TestMultiPart.cs
@@ -17,7 +17,7 @@ namespace UT_WammerStation
             FileStream f = new FileStream("SimpleMultiPartContent.txt", FileMode.Open);
             Wammer.MultiPart.Parser parser = new Wammer.MultiPart.Parser("simple boundary");
             Wammer.MultiPart.Part[] parts = parser.Parse(f);
-            Assert.AreEqual(22, parts.Length);
+            Assert.AreEqual(2, parts.Length);
             Assert.AreEqual(
                 "This is implicitly typed plain ASCII text.\r\n" +
                 "It does NOT end with a linebreak.",
@@ -26,7 +26,7 @@ namespace UT_WammerStation
             Assert.AreEqual(
                 "This is implicitly typed plain ASCII text.\r\n" +
                 "It DOES end with a linebreak.\r\n",
-                parts[0].Text);
+                parts[1].Text);
         }
     }
 }
