520d8a6b8a39f500f2c7c4af52020b8332ff85eb
Shawn Liang <shawn.liang@waveface.com>
2011-11-01 18:09:35 +0800

    attachment: implement attachments/get API

---
+++ UT_WammerStation/TestAttachmentGet.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Wammer.Station;
+using System.Net;
+using System.IO;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using Wammer.Cloud;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+
+namespace UT_WammerStation
+{
+	/// <summary>
+	/// Summary description for TestAttachmentGet
+	/// </summary>
+	[TestClass]
+	public class TestAttachmentGet
+	{
+		static MongoServer mongodb;
+		static MongoDatabase wammerDb;
+		static BsonDocument doc;
+
+
+		[ClassInitialize()]
+		public static void MyClassInitialize(TestContext testContext)
+		{
+			mongodb = MongoServer.Create("mongodb://localhost:10319/safe=true");
+		}
+
+		[TestInitialize]
+		public void setUp()
+		{
+			wammerDb = mongodb.GetDatabase("wammer");
+			if (wammerDb.CollectionExists("attachments"))
+				wammerDb.DropCollection("attachments");
+
+			wammerDb.CreateCollection("attachments");
+			MongoCollection<BsonDocument> att = wammerDb.GetCollection<BsonDocument>("attachments");
+			doc = new BsonDocument
+			{
+				{"object_id", "object_id_abc"},
+				{"title", "title1"},
+				{"description", "description1"},
+			};
+
+			att.Insert(doc);
+		}
+
+		[TestCleanup]
+		public void tearDown()
+		{
+			if (wammerDb.CollectionExists("attachments"))
+				wammerDb.DropCollection("attachments");
+		}
+
+		[TestMethod]
+		public void TestGetAttachmentInfo()
+		{
+			AttachmentService svc = new AttachmentService(mongodb);
+			WebServiceHost host = new WebServiceHost(svc, new Uri("http://localhost:8080/api/"));
+			host.Open();
+
+			WebClient agent = new WebClient();
+			string output = agent.DownloadString(
+				"http://localhost:8080/api/get?object_id=object_id_abc&session_token=a&apikey=b");
+
+			Assert.AreNotEqual("", output);
+			BsonDocument result = 
+				MongoDB.Bson.Serialization.BsonSerializer.Deserialize<BsonDocument>(output);
+
+			Assert.AreEqual("object_id_abc", result["object_id"].AsString);
+			Assert.AreEqual("title1", result["title"].AsString);
+			Assert.AreEqual("description1", result["description"].AsString);
+			Assert.IsFalse(result.Contains("_id"));
+
+			host.Close();
+		}
+
+		[TestMethod]
+		public void TestGetAttachmentInfo_paramsCanBeInAnyOrder()
+		{
+			AttachmentService svc = new AttachmentService(mongodb);
+			WebServiceHost host = new WebServiceHost(svc, new Uri("http://localhost:8080/api/"));
+			host.Open();
+
+			WebClient agent = new WebClient();
+			string output = agent.DownloadString(
+				"http://localhost:8080/api/get?apikey=b&object_id=object_id_abc&session_token=a");
+
+			Assert.AreNotEqual("", output);
+			BsonDocument result =
+				MongoDB.Bson.Serialization.BsonSerializer.Deserialize<BsonDocument>(output);
+
+			Assert.AreEqual("object_id_abc", result["object_id"].AsString);
+			Assert.AreEqual("title1", result["title"].AsString);
+			Assert.AreEqual("description1", result["description"].AsString);
+			Assert.IsFalse(result.Contains("_id"));
+
+			host.Close();
+		}
+
+		[TestMethod]
+		public void TestGetAttachmentInfo_paramCanBeOptional()
+		{
+			AttachmentService svc = new AttachmentService(mongodb);
+			WebServiceHost host = new WebServiceHost(svc, new Uri("http://localhost:8080/api/"));
+			host.Open();
+
+			WebClient agent = new WebClient();
+			string output = agent.DownloadString(
+				"http://localhost:8080/api/get?apikey=b&object_id=object_id_abc");
+
+			Assert.AreNotEqual("", output);
+			BsonDocument result =
+				MongoDB.Bson.Serialization.BsonSerializer.Deserialize<BsonDocument>(output);
+
+			Assert.AreEqual("object_id_abc", result["object_id"].AsString);
+			Assert.AreEqual("title1", result["title"].AsString);
+			Assert.AreEqual("description1", result["description"].AsString);
+			Assert.IsFalse(result.Contains("_id"));
+
+			host.Close();
+		}
+	}
+}
---
+++ Wammer.Station.Service/Station/AttachmentService.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.IO;
+
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Driver;
+using MongoDB.Driver.Builders;
+
+namespace Wammer.Station
+{
+	[ServiceContract]
+	public interface IAttatchmentService
+	{
+		[OperationContract]
+		[WebGet(UriTemplate = "get?object_id={objId}&session_token={token}&apikey={apiKey}")]
+		Stream GetAttachmentInfo(string objId, string token, string apiKey);
+	}
+
+	[ServiceBehavior(
+		InstanceContextMode=InstanceContextMode.Single,
+		ConcurrencyMode=ConcurrencyMode.Multiple)]
+	public class AttachmentService: IAttatchmentService
+	{
+		private readonly MongoServer mongodb;
+		private readonly MongoCollection<BsonDocument> attachments;
+
+		public AttachmentService(MongoServer mongo)
+		{
+			this.mongodb = mongo;
+
+			if (!mongo.GetDatabase("wammer").CollectionExists("attachments"))
+				mongo.GetDatabase("wammer").CreateCollection("attachments");
+
+			this.attachments = mongo.GetDatabase("wammer").
+														GetCollection<BsonDocument>("attachments");
+		}
+
+		public Stream GetAttachmentInfo(string object_id, string session_token, string apikey)
+		{
+			BsonDocument doc = attachments.FindOne(Query.EQ("object_id", object_id));
+			doc.Remove("_id");
+
+			MemoryStream s = new MemoryStream();
+			StreamWriter w = new StreamWriter(s);
+
+			w.Write(doc.ToJson());
+			w.Flush();
+
+			WebOperationContext.Current.OutgoingResponse.ContentType = "application/json";
+			s.Position = 0;
+			return s;
+		}
+	}
+}
