6463311246d0d4bf1b43e28b47a713065d387513
Ren Cheng <ren.cheng@waveface.com>
2011-11-28 12:13:11 +0800

    New Sign-in Dialog

---
+++ Client/StationSetup/RemoveStationForm.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Wammer.Station
+{
+    partial class RemoveStationForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelName = new System.Windows.Forms.Label();
+            this.labelLocation = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.buttonYes = new System.Windows.Forms.Button();
+            this.buttonNo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.BackColor = System.Drawing.SystemColors.Window;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(337, 19);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "You have a registered Station already";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.BackColor = System.Drawing.SystemColors.Window;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(47, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 19);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Name:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.BackColor = System.Drawing.SystemColors.Window;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(36, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 19);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Location:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelName.Location = new System.Drawing.Point(105, 40);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(0, 16);
+            this.labelName.TabIndex = 6;
+            // 
+            // labelLocation
+            // 
+            this.labelLocation.AutoSize = true;
+            this.labelLocation.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelLocation.Location = new System.Drawing.Point(105, 68);
+            this.labelLocation.Name = "labelLocation";
+            this.labelLocation.Size = new System.Drawing.Size(0, 16);
+            this.labelLocation.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.BackColor = System.Drawing.SystemColors.Window;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 106);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(193, 19);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Remove it?";
+            // 
+            // buttonYes
+            // 
+            this.buttonYes.Location = new System.Drawing.Point(116, 134);
+            this.buttonYes.Name = "buttonYes";
+            this.buttonYes.Size = new System.Drawing.Size(63, 27);
+            this.buttonYes.TabIndex = 9;
+            this.buttonYes.Text = "Yes";
+            this.buttonYes.UseVisualStyleBackColor = true;
+            this.buttonYes.Click += new System.EventHandler(this.buttonYes_Click);
+            // 
+            // buttonNo
+            // 
+            this.buttonNo.Location = new System.Drawing.Point(185, 134);
+            this.buttonNo.Name = "buttonNo";
+            this.buttonNo.Size = new System.Drawing.Size(63, 27);
+            this.buttonNo.TabIndex = 10;
+            this.buttonNo.Text = "No";
+            this.buttonNo.UseVisualStyleBackColor = true;
+            this.buttonNo.Click += new System.EventHandler(this.buttonNo_Click);
+            // 
+            // RemoveStationForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(361, 173);
+            this.Controls.Add(this.buttonNo);
+            this.Controls.Add(this.buttonYes);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.labelLocation);
+            this.Controls.Add(this.labelName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
+            this.Font = new System.Drawing.Font("Tahoma", 9F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "RemoveStationForm";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Remove Exist Station";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Label labelLocation;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button buttonYes;
+        private System.Windows.Forms.Button buttonNo;
+    }
+}
---
+++ Client/StationSetup/SignInForm.cs
@@ -0,0 +1,121 @@
+﻿
+using System;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using Wammer.Station.Management;
+
+namespace Wammer.Station
+{
+    public partial class SignInForm : Form
+    {
+        public SignInForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonOK_Click(object sender, EventArgs e)
+        {
+            // 檢查是否都有填值
+            if ((textBoxMail.Text == string.Empty) || (textBoxPassword.Text == string.Empty))
+            {
+                MessageBox.Show("Please fill all the fields!", "Waveface", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // 檢查email格式
+            if (!TestEmailFormat(textBoxMail.Text))
+            {
+                MessageBox.Show("Invalid email format", "Waveface", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            AddUser();
+        }
+
+        private void AddUser()
+        {
+            Cursor.Current = Cursors.WaitCursor;
+
+            try
+            {
+                StationController.AddUser(textBoxMail.Text, textBoxPassword.Text);
+            }
+            catch (AuthenticationException _e)
+            {
+                Cursor.Current = Cursors.Default;
+
+                MessageBox.Show(_e.Message, "Waveface", MessageBoxButtons.OK,
+                                MessageBoxIcon.Warning);
+
+                textBoxPassword.Text = string.Empty;
+
+                return;
+            }
+            catch (UserAlreadyHasStationException _e)
+            {
+                Cursor.Current = Cursors.Default;
+
+                RemoveStationForm _form = new RemoveStationForm(_e.Id, _e.Location);
+                DialogResult _dr = _form.ShowDialog();
+
+                if (_dr == DialogResult.Yes)
+                {
+                    try
+                    {
+                        Cursor.Current = Cursors.WaitCursor;
+
+                        StationController.SignoffStation(_e.Id, textBoxMail.Text, textBoxPassword.Text);
+
+                        //重新呼叫 StationController.AddUser
+                        timerDelay.Enabled = true;
+                    }
+                    catch
+                    {
+                        Cursor.Current = Cursors.Default;
+
+                        ShowErrorDialogAndExit("Sign off Station Error!");
+                    }
+                }
+                else
+                {
+                    ShowErrorDialogAndExit("您必須移除舊的Station後, 才能安裝新的");
+                }
+            }
+            catch (StationAlreadyHasDriverException _e)
+            {
+                Cursor.Current = Cursors.Default;
+
+                ShowErrorDialogAndExit(_e.Message);
+            }
+
+            //Detect DropBox
+        }
+
+        private void ShowErrorDialogAndExit(string message)
+        {
+            MessageBox.Show(message, "Waveface", MessageBoxButtons.OK,
+                                MessageBoxIcon.Error);
+
+            Close();
+        }
+
+        public bool TestEmailFormat(string emailAddress)
+        {
+            const string _patternStrict = @"^(([^<>()[\]\\.,;:\s@\""]+"
+                                         + @"(\.[^<>()[\]\\.,;:\s@\""]+)*)|(\"".+\""))@"
+                                         + @"((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}"
+                                         + @"\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+"
+                                         + @"[a-zA-Z]{2,}))$";
+
+            Regex _reStrict = new Regex(_patternStrict);
+            return _reStrict.IsMatch(emailAddress);
+        }
+
+        private void timerDelay_Tick(object sender, EventArgs e)
+        {
+            timerDelay.Enabled = false;
+
+            AddUser();
+        }
+    }
+}
