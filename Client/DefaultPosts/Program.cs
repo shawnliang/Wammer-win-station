ce9e9c655ee8dd9adb1b76b07e5c2f3665284de3
Ren Cheng <ren.cheng@waveface.com>
2011-12-01 10:40:42 +0800

    DefaultPost bugs fixs

--- Client/DefaultPosts/Program.cs
+++ Client/DefaultPosts/Program.cs
@@ -14,21 +14,20 @@ namespace Waveface
                 Console.WriteLine("Login OK!");
 
                 //
-                 _defaultPosts.PostText("Default TextPost:" + DateTime.Now.ToString());
+                 _defaultPosts.PostText("[Default TextPost]:" + DateTime.Now.ToString());
                  Console.WriteLine("PostText OK!");
-
 
                 //
                 List<string> _photos = new List<string>();
                 _photos.Add("demo1.jpg");
-                _photos.Add("demo2.jpg");
-                _defaultPosts.PostPhotos("Default Photos Post:" + DateTime.Now.ToString(), _photos);
+                //_photos.Add("demo2.jpg");
+                _defaultPosts.PostPhotos("[Default Photos Post]:" + DateTime.Now.ToString(), _photos);
                 Console.WriteLine("PostPhotos OK!");
 
-
+                //
                 List<string> _docs = new List<string>();
-                _docs.Add("demo1.pdf");
-                _defaultPosts.PostDocs("Default Docs Post:" + DateTime.Now.ToString(), _docs);
+                _docs.Add("demo.pdf");
+                _defaultPosts.PostDocs("[Default Docs Post]:" + DateTime.Now.ToString(), _docs);
                 Console.WriteLine("PostDocs OK!");
             }
             else
