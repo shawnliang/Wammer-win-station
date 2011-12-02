aa5dc477b711748372f21620fae1fc26fbf605c0
Ren Cheng <ren.cheng@waveface.com>
2011-12-02 17:23:11 +0800

    Dropbox ....

---
+++ Client/StationSetup/Dropbox/DropboxForm.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Windows.Forms;
+using Wammer.Station.Management;
+
+namespace Wammer.Station
+{
+    public partial class DropboxForm : Form
+    {
+        public DropboxForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonUserDropbox_Click(object sender, System.EventArgs e)
+        {
+            Hide();
+
+            if (!CheckDropboxInstalled())
+            {
+                DropboxInstallForm _installForm = new DropboxInstallForm();
+                DialogResult _dr = _installForm.ShowDialog();
+
+                if(_dr != DialogResult.Yes)
+                {
+                    SetupCompletedForm _form = new SetupCompletedForm();
+                    _form.ShowDialog();
+                }
+            }
+
+            DropboxLinkForm _linkForm = new DropboxLinkForm();
+            _linkForm.ShowDialog();
+        }
+
+        private bool CheckDropboxInstalled()
+        {
+            // ----------------------------
+            return false;
+
+            List<StorageStatus> _storageStatuses = StationController.ListCloudStorage();
+
+            if (_storageStatuses.Count == 0)
+                return false;
+
+            foreach (StorageStatus _status in _storageStatuses)
+            {
+                if (_status.type == "dropbox")
+                    return true;
+            }
+
+            return false;
+        }
+
+        private void buttonSkip_Click(object sender, System.EventArgs e)
+        {
+            Hide();
+
+            SetupCompletedForm _form = new SetupCompletedForm();
+            _form.ShowDialog();
+        }
+    }
+}
--- Client/StationSetup/Program.cs
+++ Client/StationSetup/Program.cs
@@ -9,55 +9,60 @@ using Waveface.Localization;
 
 namespace StationSetup
 {
-	static class Program
-	{
-		[STAThread]
-		static void Main()
-		{
-			CultureManager.ApplicationUICulture = CultureInfo.CurrentCulture;
-			//CultureManager.ApplicationUICulture = new CultureInfo("en-US");
-			//CultureManager.ApplicationUICulture = new CultureInfo("zh-TW");
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            //CultureManager.ApplicationUICulture = CultureInfo.CurrentCulture;
+            CultureManager.ApplicationUICulture = new CultureInfo("en-US");
+            //CultureManager.ApplicationUICulture = new CultureInfo("zh-TW");
 
-			Application.EnableVisualStyles();
-			Application.SetCompatibleTextRenderingDefault(false);
-			  
-			if (WavefaceWindowsClientHelper.IsAlreadyResistered())
-			{
-				WavefaceWindowsClientHelper.StartWavefaceWindowsClient("", "");
-				return;
-			}
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
 
-			Application.Run(new SignInForm());
-		}
-	}
+            /* +
+            if (WavefaceWindowsClientHelper.IsAlreadyResistered())
+            {
+                WavefaceWindowsClientHelper.StartWavefaceWindowsClient("", "");
+                return;
+            }
 
-	public class WavefaceWindowsClientHelper
-	{
-		public static bool IsAlreadyResistered()
-		{
-			return Wammer.Station.Management.StationController.GetOwner() != null;
-		}
+            Application.Run(new SignInForm());
+            */
 
-		public static void StartWavefaceWindowsClient(string email, string password)
-		{
-			string ProgramDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-			string WavefaceWindowsClientPath = Path.Combine(ProgramDir, "WavefaceWindowsClient.exe");
+            //Test
+            Application.Run(new DropboxForm());
+        }
+    }
 
-			try
-			{
-			    if ((email == string.Empty) || (password == string.Empty))
-			    {
-					Process.Start(WavefaceWindowsClientPath, null);
-			    }
-			    else
-			    {
-			        Process.Start(WavefaceWindowsClientPath, email + " " + password);
-			    }
-			}
-			catch (Exception e)
-			{
-				MessageBox.Show("Unable to start " + WavefaceWindowsClientPath, "Program error");
-			}
-		}
-	}
+    public class WavefaceWindowsClientHelper
+    {
+        public static bool IsAlreadyResistered()
+        {
+            return Wammer.Station.Management.StationController.GetOwner() != null;
+        }
+
+        public static void StartWavefaceWindowsClient(string email, string password)
+        {
+            string ProgramDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string WavefaceWindowsClientPath = Path.Combine(ProgramDir, "WavefaceWindowsClient.exe");
+
+            try
+            {
+                if ((email == string.Empty) || (password == string.Empty))
+                {
+                    Process.Start(WavefaceWindowsClientPath, null);
+                }
+                else
+                {
+                    Process.Start(WavefaceWindowsClientPath, email + " " + password);
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Unable to start " + WavefaceWindowsClientPath, "Program error");
+            }
+        }
+    }
 }
