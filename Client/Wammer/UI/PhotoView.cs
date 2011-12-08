60303cec502e1c4df160f051511f6382f5470640
Ren Cheng <ren.cheng@waveface.com>
2011-12-08 15:25:16 +0800

    Save pics

--- Client/Wammer/UI/PhotoView.cs
+++ Client/Wammer/UI/PhotoView.cs
@@ -1,10 +1,12 @@
 ﻿#region
 
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Windows.Forms;
 using Manina.Windows.Forms;
 using Waveface.Component;
+using View = Manina.Windows.Forms.View;
 
 #endregion
 
@@ -12,21 +14,26 @@ namespace Waveface
 {
     public partial class PhotoView : Form
     {
+        private Dictionary<string, string> m_filesMapping;
+        private ImageListViewItem m_selectedImage;
+
         public PhotoView()
         {
             InitializeComponent();
         }
 
-        public PhotoView(List<string> files, string fileName)
+        public PhotoView(List<string> files, Dictionary<string, string> filesMapping, string fileName)
         {
             InitializeComponent();
+
+            m_filesMapping = filesMapping;
 
             foreach (string _file in files)
             {
                 imageListView.Items.Add(_file);
             }
 
-            imageListView.View = Manina.Windows.Forms.View.Gallery;
+            imageListView.View = View.Gallery;
 
             imageListView.SetRenderer(new MyImageListViewRenderer());
 
@@ -38,40 +45,55 @@ namespace Waveface
                     return;
                 }
             }
+
+            if (imageListView.Items.Count > 0)
+                imageListView.Items[0].Selected = true;
+        }
+
+        private void imageListView_SelectionChanged(object sender, EventArgs e)
+        {
+            if (imageListView.SelectedItems.Count != 0)
+            {
+                m_selectedImage = imageListView.SelectedItems[0];
+            }
         }
 
         #region Save
 
-        private void miSave_Click(object sender, System.EventArgs e)
+        private void miSave_Click(object sender, EventArgs e)
         {
             SavePic();
         }
 
-        private void miSaveAll_Click(object sender, System.EventArgs e)
+        private void miSaveAll_Click(object sender, EventArgs e)
         {
             SaveAllPics();
         }
 
-        private void btnSave_Click(object sender, System.EventArgs e)
+        private void btnSave_Click(object sender, EventArgs e)
         {
             SavePic();
         }
 
-        private void btnSaveAll_Click(object sender, System.EventArgs e)
+        private void btnSaveAll_Click(object sender, EventArgs e)
         {
             SaveAllPics();
         }
 
         private void SavePic()
         {
-            if (imageListView.SelectedItems.Count == 0)
+            if (imageListView.SelectedItems.Count != 0)
             {
-                return;
+                m_selectedImage = imageListView.SelectedItems[0];
             }
 
-            string _picFile = imageListView.SelectedItems[0].FileName;
+            string _picFilePath = m_selectedImage.FileName;
+            string _trueName = new FileInfo(_picFilePath).Name;
 
-            saveFileDialog.FileName = new FileInfo(_picFile).Name;
+            if (m_filesMapping.ContainsKey(_trueName)) //取得原始檔名
+                _trueName = m_filesMapping[_trueName];
+
+            saveFileDialog.FileName = _trueName;
             DialogResult _dr = saveFileDialog.ShowDialog();
 
             if (_dr == DialogResult.OK)
@@ -80,7 +102,7 @@ namespace Waveface
                 {
                     string _destFile = saveFileDialog.FileName;
 
-                    File.Copy(_picFile, _destFile);
+                    File.Copy(_picFilePath, _destFile);
 
                     MessageBox.Show("File Save Successful!");
                 }
@@ -93,7 +115,36 @@ namespace Waveface
 
         private void SaveAllPics()
         {
+            string _fileName = string.Empty;
 
+            using (FolderBrowserDialog _dialog = new FolderBrowserDialog())
+            {
+                _dialog.Description = "Select location to save files";
+                _dialog.ShowNewFolderButton = true;
+                _dialog.RootFolder = Environment.SpecialFolder.Desktop;
+
+                if (_dialog.ShowDialog() == DialogResult.OK)
+                {
+                    string _folder = _dialog.SelectedPath + "\\";
+
+                    foreach (ImageListViewItem _item in imageListView.Items)
+                    {
+                        _fileName = m_filesMapping[new FileInfo(_item.FileName).Name]; // 取出真實名稱
+                        _fileName = FileUtility.saveFileWithoutOverwrite(_fileName, _folder);
+
+                        try
+                        {
+                            File.Copy(_item.FileName, _fileName);
+                        }
+                        catch
+                        {
+                        }
+                    }
+
+                    MessageBox.Show("All images are saved.", "Waveface", MessageBoxButtons.OK,
+                                    MessageBoxIcon.Information);
+                }
+            }
         }
 
         #endregion
