96fc0aaa852aaebb8c1b30eefc1095f922c5c2ab
Kevin Chiu <kevin.chiu@waveface.com>
2011-12-12 14:06:48 +0800

    add a management http server on 9989

---
+++ Station/Wammer.Station.Service/Station/StationOfflineHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Wammer.Station
+{
+	public class StationOfflineHandler : HttpHandler
+	{
+		private readonly HttpServer functionalServer;
+
+		public StationOfflineHandler(HttpServer functionalServer)
+		{
+			this.functionalServer = functionalServer;
+		}
+
+		protected override void HandleRequest()
+		{
+		}
+
+		public override object Clone()
+		{
+			return this.MemberwiseClone();
+		}
+	}
+}
