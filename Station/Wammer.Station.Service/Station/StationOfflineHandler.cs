fadaa03e057c74abc61277fde8ec2a070f224bbd
Shawn Liang <shawn.liang@waveface.com>
2011-12-12 15:20:23 +0800

    implement offline handler

--- Station/Wammer.Station.Service/Station/StationOfflineHandler.cs
+++ Station/Wammer.Station.Service/Station/StationOfflineHandler.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using Wammer.Model;
+using MongoDB.Driver.Builders;
+
 namespace Wammer.Station
 {
 	public class StationOfflineHandler : HttpHandler
@@ -16,6 +19,41 @@ namespace Wammer.Station
 
 		protected override void HandleRequest()
 		{
+			string session_token = Parameters["session_token"];
+
+			if (session_token == null)
+				throw new FormatException("session_token is missing");
+
+			LogOutStationFromCloud(session_token);
+			
+			functionalServer.Stop();
+
+			WriteOfflineStateToDB();
+
+			RespondSuccess();
+		}
+
+		private static void WriteOfflineStateToDB()
+		{
+			Model.Service svc = ServiceCollection.FindOne(Query.EQ("_id", "StationService"));
+			if (svc == null)
+			{
+				svc = new Model.Service { Id = "StationService", State = ServiceState.Offline };
+			}
+			else
+				svc.State = ServiceState.Offline;
+
+			ServiceCollection.Save(svc);
+		}
+
+		private static void LogOutStationFromCloud(string session_token)
+		{
+			Model.StationInfo station = Model.StationInfo.collection.FindOne();
+			if (station == null)
+				throw new InvalidOperationException("station is null in station collection");
+
+			Cloud.StationApi stationApi = new Cloud.StationApi(station.Id, session_token);
+			stationApi.Offline(new System.Net.WebClient());
 		}
 
 		public override object Clone()
