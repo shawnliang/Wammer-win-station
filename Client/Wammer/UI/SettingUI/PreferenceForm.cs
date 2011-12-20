f82ad3a0d32674f1f12b22e173e9a414f5d10f5e
Kevin Chiu <kevin.chiu@waveface.com>
2011-12-20 10:48:49 +0800

    Add preference option to system tray icon

--- Client/Wammer/UI/SettingUI/PreferenceForm.cs
+++ Client/Wammer/UI/SettingUI/PreferenceForm.cs
@@ -15,11 +15,6 @@ namespace Waveface.SettingUI
 
         }
 
-        private void ckbAllowDropbox_CheckedChanged(object sender, EventArgs e)
-        {
-            panelLimitSpace.Enabled = ckbAllowDropbox.Checked;
-        }
-
         private void buttonOK_Click(object sender, EventArgs e)
         {
             DialogResult = DialogResult.OK;
@@ -29,22 +24,6 @@ namespace Waveface.SettingUI
         private void buttonCancel_Click(object sender, EventArgs e)
         {
             Close();
-        }
-
-        private void buttonChangeFolder_Click(object sender, EventArgs e)
-        {
-            using (FolderBrowserDialog _dialog = new FolderBrowserDialog())
-            {
-                _dialog.Description = "Default Station Folder";
-                _dialog.ShowNewFolderButton = true;
-                _dialog.RootFolder = Environment.SpecialFolder.Desktop;
-                
-                if (_dialog.ShowDialog() == DialogResult.OK)
-                {
-                    string _folder = _dialog.SelectedPath;
-                    labelDataPath.Text = _folder;
-                }
-            }
         }
     }
 }
