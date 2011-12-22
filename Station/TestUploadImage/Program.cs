1d70e5ddf4260d26a55dc65257112f5516ddbb2d
Shawn Liang <shawn.liang@waveface.com>
2011-12-22 10:40:35 +0800

    add test program

---
+++ Station/TestUploadImage/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+
+namespace TestUploadImage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Arguments arguments = new Arguments(args);
+
+                string image_file = arguments["--image-file"];
+                int threads = (arguments["--threads"] != null) ? int.Parse(arguments["--threads"]) : 1;
+                int uploads_per_thread = (arguments["--uploads-per-thread"] != null) ? int.Parse(arguments["--uploads-per-thread"]) : 100;
+
+                if (image_file == null)
+                    throw new ArgumentException("--image-file is not specified");
+
+
+
+
+            }
+            catch (ArgumentException e)
+            {
+                PrintUsage();
+                Console.WriteLine(e.Message);
+            }
+            catch (Exception e)
+            {
+                PrintUsage();
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+
+        static void PrintUsage()
+        {
+            Console.WriteLine("TestUploadImage usages:");
+            Console.WriteLine();
+            Console.WriteLine("--image-file path        image file to upload.");
+            Console.WriteLine("--threads n              upload thread numbers. default = 1");
+            Console.WriteLine("--uploads-per-thread n   upload count per thread. default = 100");
+            Console.WriteLine();
+        }
+    }
+
+
+    class Arguments
+    {
+        NameValueCollection parameters;
+
+        public Arguments(string[] args)
+        {
+            int i = 0;
+            while (i < args.Length)
+            {
+                if (args[i].StartsWith("--"))
+                {
+                    if (i + 1 < args.Length && !args[i + 1].StartsWith("--"))
+                    {
+                        parameters[args[i]] = args[i + 1];
+                        ++i;
+                    }
+                    else
+                    {
+                        parameters[args[i]] = "";
+                    }
+                }
+                else
+                {
+                    throw new ArgumentException("invalid argument: " + args[i]);
+                }
+
+                ++i;
+            }
+        }
+
+        public string this[string key]
+        {
+            get { return parameters[key]; }
+        }
+
+        public string[] AllKeys
+        {
+            get { return parameters.AllKeys; }
+        }
+    }
+}
---
+++ Station/TestUploadImage/UploadThread.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Wammer.Model;
+
+namespace TestUploadImage
+{
+    class UploadThread
+    {
+        byte[] image_data;
+        int upload_count;
+        ManualResetEvent start_event;
+        string session_token;
+        string group_id;
+
+        public UploadThread(int upload_count, byte[] image_data, ManualResetEvent start_event, string session_token, string group_id)
+        {
+            this.image_data = image_data;
+            this.upload_count = upload_count;
+            this.start_event = start_event;
+            this.session_token = session_token;
+            this.group_id = group_id;
+        }
+
+        private void Do()
+        {
+            if (!start_event.WaitOne())
+                throw new InvalidOperationException("start_event is not waitable");
+
+            for (int i = 0; i < upload_count; i++)
+            {
+                try
+                {
+                    Attachment.UploadImage("http://localhost:9981/v2/attachments/upload",
+                        image_data, 
+                        group_id, 
+                        null, 
+                        "hard_code_file_from_test_prog.jpg", 
+                        "image/jpeg", 
+                        ImageMeta.Origin,
+                        "api-key", 
+                        session_token);
+                }
+                catch (Exception e)
+                {
+
+                }
+
+            }
+        }
+    }
+}
