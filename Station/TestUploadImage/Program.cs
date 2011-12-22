afddf71e1e7cfc5e975b41761b384cedc3aafff1
Shawn Liang <shawn.liang@waveface.com>
2011-12-22 15:33:42 +0800

    implement performance test

--- Station/TestUploadImage/Program.cs
+++ Station/TestUploadImage/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
-using System.Linq;
+using System.Net;
 using System.Text;
+using System.IO;
+using System.Threading;
+using Wammer.Utility;
+using Wammer.Cloud;
 
 namespace TestUploadImage
 {
@@ -18,12 +22,77 @@ namespace TestUploadImage
                 int threads = (arguments["--threads"] != null) ? int.Parse(arguments["--threads"]) : 1;
                 int uploads_per_thread = (arguments["--uploads-per-thread"] != null) ? int.Parse(arguments["--uploads-per-thread"]) : 100;
 
+                string email = arguments["--email"];
+                string password = arguments["--password"];
+                string station = arguments["--station"];
+
+
                 if (image_file == null)
                     throw new ArgumentException("--image-file is not specified");
+                if (email == null)
+                    throw new ArgumentException("--email is not specified");
+                if (password == null)
+                    throw new ArgumentException("--password is not specified");
+
+                if (station == null)
+                    station = "127.0.0.1";
+
+                User user = User.LogIn(new WebClient(), email, password);
+
+                byte[] image_data = LoadImage(image_file);
+
+                ManualResetEvent start_event = new ManualResetEvent(false);
+
+
+                // Start threads to upload images
+                List<UploadThread> workers = new List<UploadThread>();
+                for (int i = 0; i < threads; i++)
+                {
+                    UploadThread thread = new UploadThread(uploads_per_thread, image_data, start_event, user.Token, user.Groups[0].group_id, station);
+                    workers.Add(thread);
+                    thread.Start();
+                }
+
+                start_event.Set();
+
+
+                // Waiting upload complete
+                for (int i = 0; i < threads; i++)
+                {
+                    workers[i].Join();
+                }
 
 
 
+                
+                // Compute statistics
+                int total_success = 0;
+                int total_error = 0;
+                TimeSpan total_duration_of_success = TimeSpan.FromTicks(0);
 
+                for (int i = 0; i < threads; i++)
+                {
+                    int success_count = workers[i].success_count;
+                    int error_count = workers[i].error_count;
+                    TimeSpan success_duration = workers[i].total_duration;
+
+                    string text = string.Format("Worker {0}: success {1}, success duration {2}, failure {3}",
+                        i, success_count, success_duration, error_count);
+
+                    Console.WriteLine(text);
+
+                    total_success += success_count;
+                    total_error += error_count;
+                    total_duration_of_success += success_duration;
+                }
+
+                Console.WriteLine("===================================");
+                Console.WriteLine("total success: " + total_success);
+                Console.WriteLine("total error: " + total_error);
+                Console.WriteLine("total duration of success: " + total_duration_of_success.TotalMilliseconds + " ms");
+
+                Console.WriteLine("Average duration of a success upload: " + total_duration_of_success.TotalMilliseconds / total_success);
+                Console.WriteLine("Throughput : " + total_success * 1000.0 / total_duration_of_success.TotalMilliseconds + "uploads/sec");
             }
             catch (ArgumentException e)
             {
@@ -42,17 +111,29 @@ namespace TestUploadImage
         {
             Console.WriteLine("TestUploadImage usages:");
             Console.WriteLine();
-            Console.WriteLine("--image-file path        image file to upload.");
+            Console.WriteLine("--image-file path        image file to upload");
+            Console.WriteLine("--email email            waveface user email");
+            Console.WriteLine("--password pwd           waveface user password");
+
             Console.WriteLine("--threads n              upload thread numbers. default = 1");
             Console.WriteLine("--uploads-per-thread n   upload count per thread. default = 100");
+            Console.WriteLine("--station ip_or_host     station's ip or hostname. default = 127.0.0.1");
+
             Console.WriteLine();
+        }
+
+        static byte[] LoadImage(string filename)
+        {
+            MemoryStream m = new MemoryStream();
+            StreamHelper.Copy(File.OpenRead(filename), m);
+            return m.ToArray();
         }
     }
 
 
     class Arguments
     {
-        NameValueCollection parameters;
+        NameValueCollection parameters = new NameValueCollection();
 
         public Arguments(string[] args)
         {
