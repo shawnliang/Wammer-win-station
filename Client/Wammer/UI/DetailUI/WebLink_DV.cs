f6e20c9e3c85c54a39f388dd5907664f7a51976e
Ren Cheng <ren.cheng@waveface.com>
2012-02-02 15:33:35 +0800

    Web Post (2)

--- Client/Wammer/UI/DetailUI/WebLink_DV.cs
+++ Client/Wammer/UI/DetailUI/WebLink_DV.cs
@@ -11,7 +11,7 @@ using Waveface.Component;
 
 namespace Waveface.DetailUI
 {
-    public class Text_Link_DV : UserControl, IDetailViewer
+    public class WebLink_DV : UserControl, IDetailViewer
     {
         private IContainer components;
         private Panel panelMain;
@@ -21,11 +21,7 @@ namespace Waveface.DetailUI
         private WebBrowser webBrowserComment;
         private Post m_post;
         private XPButton buttonAddComment;
-        private WebBrowser webBrowser;
         private Panel panelWebBrowser;
-        private TabControl tabControl;
-        private TabPage tabPageSoul;
-        private TabPage tabPageWebPage;
         private WebBrowser webBrowserSoul;
         private Localization.CultureManager cultureManager;
         private TextBox textBoxComment;
@@ -49,7 +45,7 @@ namespace Waveface.DetailUI
 
         public DetailView MyParent { get; set; }
 
-        public Text_Link_DV()
+        public WebLink_DV()
         {
             InitializeComponent();
         }
@@ -78,17 +74,13 @@ namespace Waveface.DetailUI
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Text_Link_DV));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WebLink_DV));
             this.panelMain = new System.Windows.Forms.Panel();
             this.panelRight = new System.Windows.Forms.Panel();
             this.PanelAddComment = new System.Windows.Forms.Panel();
             this.textBoxComment = new System.Windows.Forms.TextBox();
             this.webBrowserComment = new System.Windows.Forms.WebBrowser();
             this.panelWebBrowser = new System.Windows.Forms.Panel();
-            this.tabControl = new System.Windows.Forms.TabControl();
-            this.tabPageWebPage = new System.Windows.Forms.TabPage();
-            this.webBrowser = new System.Windows.Forms.WebBrowser();
-            this.tabPageSoul = new System.Windows.Forms.TabPage();
             this.webBrowserSoul = new System.Windows.Forms.WebBrowser();
             this.webBrowserTop = new System.Windows.Forms.WebBrowser();
             this.cultureManager = new Waveface.Localization.CultureManager(this.components);
@@ -97,9 +89,6 @@ namespace Waveface.DetailUI
             this.panelRight.SuspendLayout();
             this.PanelAddComment.SuspendLayout();
             this.panelWebBrowser.SuspendLayout();
-            this.tabControl.SuspendLayout();
-            this.tabPageWebPage.SuspendLayout();
-            this.tabPageSoul.SuspendLayout();
             this.SuspendLayout();
             // 
             // panelMain
@@ -142,39 +131,10 @@ namespace Waveface.DetailUI
             // 
             // panelWebBrowser
             // 
-            this.panelWebBrowser.BackColor = System.Drawing.SystemColors.AppWorkspace;
-            this.panelWebBrowser.Controls.Add(this.tabControl);
+            this.panelWebBrowser.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(242)))), ((int)(((byte)(238)))));
+            this.panelWebBrowser.Controls.Add(this.webBrowserSoul);
             resources.ApplyResources(this.panelWebBrowser, "panelWebBrowser");
             this.panelWebBrowser.Name = "panelWebBrowser";
-            // 
-            // tabControl
-            // 
-            resources.ApplyResources(this.tabControl, "tabControl");
-            this.tabControl.Controls.Add(this.tabPageWebPage);
-            this.tabControl.Controls.Add(this.tabPageSoul);
-            this.tabControl.Name = "tabControl";
-            this.tabControl.SelectedIndex = 0;
-            // 
-            // tabPageWebPage
-            // 
-            this.tabPageWebPage.Controls.Add(this.webBrowser);
-            resources.ApplyResources(this.tabPageWebPage, "tabPageWebPage");
-            this.tabPageWebPage.Name = "tabPageWebPage";
-            this.tabPageWebPage.UseVisualStyleBackColor = true;
-            // 
-            // webBrowser
-            // 
-            resources.ApplyResources(this.webBrowser, "webBrowser");
-            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
-            this.webBrowser.Name = "webBrowser";
-            this.webBrowser.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser_DocumentCompleted);
-            // 
-            // tabPageSoul
-            // 
-            this.tabPageSoul.Controls.Add(this.webBrowserSoul);
-            resources.ApplyResources(this.tabPageSoul, "tabPageSoul");
-            this.tabPageSoul.Name = "tabPageSoul";
-            this.tabPageSoul.UseVisualStyleBackColor = true;
             // 
             // webBrowserSoul
             // 
@@ -182,6 +142,8 @@ namespace Waveface.DetailUI
             this.webBrowserSoul.MinimumSize = new System.Drawing.Size(20, 20);
             this.webBrowserSoul.Name = "webBrowserSoul";
             this.webBrowserSoul.ScriptErrorsSuppressed = true;
+            this.webBrowserSoul.ScrollBarsEnabled = false;
+            this.webBrowserSoul.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowserSoul_DocumentCompleted);
             // 
             // webBrowserTop
             // 
@@ -205,19 +167,16 @@ namespace Waveface.DetailUI
             this.buttonAddComment.UseVisualStyleBackColor = true;
             this.buttonAddComment.Click += new System.EventHandler(this.buttonAddComment_Click);
             // 
-            // Text_Link_DV
+            // WebLink_DV
             // 
             this.Controls.Add(this.panelMain);
             resources.ApplyResources(this, "$this");
-            this.Name = "Text_Link_DV";
+            this.Name = "WebLink_DV";
             this.panelMain.ResumeLayout(false);
             this.panelRight.ResumeLayout(false);
             this.PanelAddComment.ResumeLayout(false);
             this.PanelAddComment.PerformLayout();
             this.panelWebBrowser.ResumeLayout(false);
-            this.tabControl.ResumeLayout(false);
-            this.tabPageWebPage.ResumeLayout(false);
-            this.tabPageSoul.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -235,17 +194,20 @@ namespace Waveface.DetailUI
 
         private void Set_Comments_Part()
         {
-           MyParent.SetComments(webBrowserComment, Post, true);
+            MyParent.SetComments(webBrowserComment, Post, true);
         }
 
         private void Set_MainContent_Preview_Part()
         {
             StringBuilder _sb = new StringBuilder();
 
-            _sb.Append("<font face='微軟正黑體, Helvetica, Arial, Verdana, sans-serif'><p>[Text]</p></font>");
+            if (Post.preview.url != null)
+                _sb.Append("<font face='微軟正黑體, Helvetica, Arial, Verdana, sans-serif' color='#eef'><p>[Text]</p><hr><br></font>");
+            else
+                _sb.Append("<font face='微軟正黑體, Helvetica, Arial, Verdana, sans-serif' color='#eef'><p>[Text]</p><br></font>");
 
             string _html = _sb.ToString();
-            
+
             string _content = Post.content.Replace(Environment.NewLine, "<BR>");
             _content = _content.Replace("\n", "<BR>");
 
@@ -258,34 +220,29 @@ namespace Waveface.DetailUI
                 Preview_OpenGraph _p = Post.preview;
                 StringBuilder _s = new StringBuilder();
 
-                _s.Append("	<table border=\"0\">");
-                _s.Append("    	   <tr>");
-                _s.Append("      	     <td><a href=\"[PriviewLink]\" target=\"_blank\"><img src=\"[PreviewPic]\" width=\"180\" height=\"120\" /></td>");
-                _s.Append("      	     <td>");
-                _s.Append(" 		<table border=\"0\">");
-                _s.Append("    			<tr>");
-                _s.Append("      				<td><a href=\"[PriviewLink]\" target=\"_blank\">[PriviewTitle]</a></td>");
-                _s.Append("    			</tr>");
-                _s.Append("    			<tr>");
-                _s.Append("      				<td>[PriviewText]</td>");
-                _s.Append("    			</tr>");
-                _s.Append("		</table>");
-                _s.Append("      	     </td>");
-                _s.Append("    	  </tr>");
-                _s.Append("	</table>");
+                _s.Append("<table width=\"99%\" border=\"0\">");
+
+                _s.Append(" <tr>");
+                _s.Append("     <td><font face='微軟正黑體, Helvetica, Arial, Verdana, sans-serif' size='5pt'><b><a style=\"text-decoration:none\" href=\"[PriviewLink]\" target=\"_blank\">[PriviewTitle]</a></b></font></td>");
+                _s.Append(" </tr>");
+
+                _s.Append(" <tr>");
+                _s.Append("     <td><font size='2.75pt'>[PriviewLink2]</font></td>");
+                _s.Append(" </tr>");
+
+                _s.Append("</table>");
 
                 _html += _s.ToString();
 
                 _html = _html.Replace("[PreviewPic]", _p.thumbnail_url);
                 _html = _html.Replace("[PriviewTitle]", _p.title);
                 _html = _html.Replace("[PriviewLink]", _p.url);
+                _html = _html.Replace("[PriviewLink2]", StringUtility.ExtractDomainNameFromURL(_p.url));
                 _html = _html.Replace("[PriviewText]", _p.description);
-
-                ShowWebBrowser(Post.preview.url);
             }
 
-            webBrowserTop.DocumentText = HtmlUtility.TrimScript("<body bgcolor=\"rgb(243, 242, 238)\">" + _html + "</body>");
-            webBrowserSoul.DocumentText = HtmlUtility.TrimScript(m_post.soul);
+            webBrowserTop.DocumentText = HtmlUtility.TrimScript("<body bgcolor=\"rgb(243, 242, 238)\">" + _html + "</quote></body>");
+            webBrowserSoul.DocumentText = HtmlUtility.TrimScript("<body bgcolor=\"rgb(243, 242, 238)\"><font face='微軟正黑體, Helvetica, Arial, Verdana, sans-serif'>" + m_post.soul + "</font></body>"); //"<body bgcolor=\"rgb(243, 242, 238)\">" + m_post.soul + "</body>"
         }
 
         private void webBrowserTop_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
@@ -293,17 +250,11 @@ namespace Waveface.DetailUI
             int _h = webBrowserTop.Document.Body.ScrollRectangle.Height;
             webBrowserTop.Height = _h;
         }
-         
+
         private void webBrowserComment_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
         {
             int _h = webBrowserComment.Document.Body.ScrollRectangle.Height;
             webBrowserComment.Height = _h;
-        }
-
-        private void ShowWebBrowser(string url)
-        {
-            webBrowser.ScriptErrorsSuppressed = true;
-            webBrowser.Navigate(url);
         }
 
         private void buttonAddComment_Click(object sender, EventArgs e)
@@ -311,14 +262,15 @@ namespace Waveface.DetailUI
             MyParent.PostComment(textBoxComment, Post);
         }
 
-        private void webBrowser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        private void webBrowserSoul_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
         {
-            int _h = webBrowser.Document.Body.ScrollRectangle.Height;
+            int _h = webBrowserSoul.Document.Body.ScrollRectangle.Height;
 
-            panelWebBrowser.Height = _h + 256;
-            webBrowser.Height = _h + 128;
+            panelWebBrowser.Height = _h + 4;
+            webBrowserSoul.Height = _h;
 
-            panelWebBrowser.Visible = true;
+            if (m_post.soul.Trim() != string.Empty)
+                panelWebBrowser.Visible = true;
         }
 
         #region IDetailViewer
