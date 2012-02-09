b87499e4a191648e3d3a6944fdd214cb0de92892
Kevin Chiu <kevin.chiu@waveface.com>
2012-02-09 16:05:21 +0800

    fix publish datetime for dev & product versioninfo.xml

--- versioninfo.py
+++ versioninfo.py
@@ -37,23 +37,22 @@ def genxml(link, title, rnote, pubdate, binfile, binurl, binlen, binver, xmlfile
 if __name__ == "__main__":
     print "Generate versioninfo.xml"
     version = sys.argv[1]
+    pubdate = datetime.datetime.utcnow().replace(microsecond=0).isoformat(' ') + '+0800'
 
     # development
     dev_link = "http://develop.waveface.com:4343/extensions/windowsUpdate/versioninfo.xml"
     dev_title = "Version {0}".format(version)
     dev_rnote = "http://develop.waveface.com:4343/extensions/windowsUpdate/rnotes-{0}.html".format(version)
-    dev_pubdate = datetime.datetime.utcnow().replace(microsecond=0).isoformat(' ') + '+0800'
     dev_binfile = "development-WavefaceSetup-{0}.exe".format(version)
     dev_binurl = "http://develop.waveface.com:4343/extensions/windowsUpdate/{0}".format(dev_binfile)
     dev_binlen = os.path.getsize(os.path.join(os.path.dirname(solution_dir), dev_binfile))
     dev_binver = version
-    genxml(dev_link, dev_title, dev_rnote, dev_pubdate, dev_binfile, dev_binurl, dev_binlen, dev_binver, "versioninfo_dev.xml")
+    genxml(dev_link, dev_title, dev_rnote, pubdate, dev_binfile, dev_binurl, dev_binlen, dev_binver, "versioninfo_dev.xml")
 
     # production
     link = "https://waveface.com/extensions/windowsUpdate/versioninfo.xml"
     title = "Version {0}".format(version)
     rnote = "https://waveface.com/extensions/windowsUpdate/rnotes-{0}.html".format(version)
-    pubdate = datetime.datetime.utcnow().replace(microsecond=0).isoformat(' ') + '+0800'
     binfile = "production-WavefaceSetup-{0}.exe".format(version)
     binurl = "https://waveface.com/extensions/windowsUpdate/{0}".format(binfile)
     binlen = os.path.getsize(os.path.join(os.path.dirname(solution_dir), binfile))
