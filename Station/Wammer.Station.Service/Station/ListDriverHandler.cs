6cc19886845d6901de26f0ba21e6b8b10f2e02e9
Kevin Chiu <kevin.chiu@waveface.com>
2012-03-01 18:32:52 +0800

    add ListDriver API

---
+++ Station/Wammer.Station.Service/Station/ListDriverHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using log4net;
+
+using Wammer.Model;
+using Wammer.Cloud;
+
+namespace Wammer.Station
+{
+	public class ListDriverHandler: HttpHandler
+	{
+		private static ILog logger = LogManager.GetLogger("ListDriverHandler");
+
+		protected override void HandleRequest()
+		{
+			List<Driver> drivers = new List<Driver>(DriverCollection.Instance.FindAll());
+
+			RespondSuccess(new ListDriverResponse(drivers));
+		}
+
+		public override object Clone()
+		{
+			return this.MemberwiseClone();
+		}
+	}
+
+	public class ListDriverResponse: CloudResponse
+	{
+		public List<Driver> drivers;
+		public ListDriverResponse(List<Driver> drivers)
+		{
+			this.drivers = drivers;
+		}
+	}
+}
