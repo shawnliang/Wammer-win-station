e92030ec9b7215659672efb17948f353a3645bc4
larry nung <larry.nung@waveface.com>
2012-03-22 14:00:48 +0800

    Remove resource when account removed

--- Station/Wammer.Station.Service/Station/RemoveOwnerHandler.cs
+++ Station/Wammer.Station.Service/Station/RemoveOwnerHandler.cs
@@ -3,6 +3,7 @@ using System.Net;
 using MongoDB.Driver.Builders;
 using Wammer.Cloud;
 using Wammer.Model;
+using System.IO;
 
 namespace Wammer.Station
 {
@@ -19,6 +20,7 @@ namespace Wammer.Station
 		{
 			string stationToken = Parameters["session_token"];
             string userID = Parameters["user_ID"];
+			bool removeResource = bool.Parse(Parameters["remove_resource"]);
 
             if (stationToken == null || userID == null)
                 throw new FormatException("One of parameters is missing: email/password/session_token/userID");
@@ -27,11 +29,19 @@ namespace Wammer.Station
             Driver existingDriver = Model.DriverCollection.Instance.FindOne(Query.EQ("_id", userID));
             Boolean isDriverExists = existingDriver != null;
 
-            //If driver exists & reference count > 0 => reference count decrease one
-            if (isDriverExists && existingDriver.ref_count > 0)
+            //Driver not exists => return
+            if (!isDriverExists)
+            {
+                RespondSuccess();
+                return;
+            }
+
+            //reference count > 0 => reference count decrease one
+            if (existingDriver.ref_count > 0)
             {
                 --existingDriver.ref_count;
                 DriverCollection.Instance.Save(existingDriver);
+                RespondSuccess();
                 return;
             }
 
@@ -41,11 +51,13 @@ namespace Wammer.Station
             //Remove the user from db, and stop service this user
             Model.DriverCollection.Instance.Remove(Query.EQ("_id", userID));
 
+			if (removeResource && Directory.Exists(existingDriver.folder))
+				Directory.Delete(existingDriver.folder);
+
             //All driver removed => Remove station from db
 			Driver driver = Model.DriverCollection.Instance.FindOne();
 			if (driver == null)
 				Model.StationCollection.Instance.RemoveAll();
-
 
 			RespondSuccess();
 		}
