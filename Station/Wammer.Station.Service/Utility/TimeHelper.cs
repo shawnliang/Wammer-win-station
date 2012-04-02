36bbf0f5a75d73767a094c72daa67264d1856bc5
Kevin Chiu <kevin.chiu@waveface.com>
2012-04-02 13:56:26 +0800

    fix conversion between unix timestamp & DateTime
    use seconds instead of milliseconds.

--- Station/Wammer.Station.Service/Utility/TimeHelper.cs
+++ Station/Wammer.Station.Service/Utility/TimeHelper.cs
@@ -6,20 +6,20 @@ namespace Wammer.Utility
 	{
 		private static DateTime JAN_1_1970 = new DateTime(1970, 1, 1);
 
-		public static long GetMillisecondsSince1970()
+		public static long GetSecondsSince1970()
 		{
 			TimeSpan span = DateTime.Now - JAN_1_1970;
-			return (long)span.TotalMilliseconds;
+			return (long)span.TotalSeconds;
 		}
 
 		public static DateTime ConvertToDateTime(long unixTimeStamp)
 		{
-			return JAN_1_1970.AddMilliseconds(unixTimeStamp);
+			return JAN_1_1970.AddSeconds(unixTimeStamp);
 		}
 
 		public static long ConvertToUnixTimeStamp(DateTime datetime)
 		{
-			return (long)(datetime - JAN_1_1970).TotalMilliseconds;
+			return (long)(datetime - JAN_1_1970).TotalSeconds;
 		}
 	}
 }
