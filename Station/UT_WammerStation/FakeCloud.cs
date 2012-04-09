694cd8584b16472c11002fc7b99744e90a89323b
Shawn Liang <shawn.liang@waveface.com>
2012-04-09 15:16:41 +0800

    fix unit test failures

--- Station/UT_WammerStation/FakeCloud.cs
+++ Station/UT_WammerStation/FakeCloud.cs
@@ -134,7 +134,7 @@ namespace UT_WammerStation
 					this.resWriters.Add(new JsonResponseWriter((string)response));
 				else
 					this.resWriters.Add(
-						new JsonResponseWriter(fastJSON.JSON.Instance.ToJSON(response)));
+						new JsonResponseWriter(response.ToFastJSON()));
 			}
 		}
 
