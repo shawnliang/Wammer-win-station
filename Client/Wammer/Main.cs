94ee1a21c9337704f1689c4b5f95d6ce1806c90e
Shawn Liang <shawn.liang@waveface.com>
2012-04-11 14:21:46 +0800

    rollback windows client pulling timeline behavior
    GetLatest first and then get posts until api returned remaining_count is
    zero. Because windows client pulls timeline every 500 milliseconds, we
    don't need to pull all timeline at all in one pull.

--- Client/Wammer/Main.cs
+++ Client/Wammer/Main.cs
@@ -1697,13 +1697,7 @@ namespace Waveface
 
             try
 			{
-				// station might return no posts if it hasn't prefetch any timeline from cloud,
-				// but we can know the actual post count by total_count
-				do
-				{
-					_getLatest = RT.REST.Posts_getLatest(_firstGetCount);
-				}
-				while (_getLatest.get_count == 0 && _getLatest.total_count > 0);
+				_getLatest = RT.REST.Posts_getLatest(_firstGetCount);
 			}
 			catch
             {
@@ -1724,9 +1718,9 @@ namespace Waveface
 
                 if (_getLatest.get_count < _getLatest.total_count)
                 {
-                    int _fetchedCount = _getLatest.get_count;
+                    int _remainingCount = int.MaxValue;
 
-                    while (_getLatest.total_count > _fetchedCount)
+                    while (_remainingCount > 0)
                     {
 						if (bgWorkerGetAllData.CancellationPending)
 						{
@@ -1746,9 +1740,9 @@ namespace Waveface
                                 {
                                     _allPosts.Add(_p.post_id, _p);
                                     _datum = _p.timestamp;
-                                    _fetchedCount++;
                                 }
                             }
+                            _remainingCount = _postsGet.remaining_count;
                         }
                     }
                 }
