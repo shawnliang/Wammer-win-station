c4cf4f4e1087978aa5d75933bf08887842a95274
Shawn Liang <shawn.liang@waveface.com>
2012-04-17 15:43:10 +0800

    migrate thumbnail names from prev versions
    In the new version, thumnbnail extension is always ".dat". Older version
    of waveface station uses ".jpg", ".gif", and others which need to be
    converted to ".dat" while upgrading.

---
+++ Station/Wammer.Station.Service/Model/ProductInfo.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace Wammer.Model
+{
+	[BsonIgnoreExtraElements]
+	public class ProductInfo
+	{
+		[BsonId]
+		public BsonObjectId id { get; set; }
+
+		[BsonIgnoreIfNull]
+		[BsonDefaultValue(false)]
+		public bool ThumbnailExtensionIsDat { get; set; }
+
+
+		public static ProductInfo GetCurrentVersion()
+		{
+			return new ProductInfo
+			{
+				ThumbnailExtensionIsDat = true
+			};
+		}
+	}
+
+	public class ProductInfoCollection: Collection<ProductInfo>
+	{
+		private static ProductInfoCollection instance;
+
+		static ProductInfoCollection()
+		{
+			instance = new ProductInfoCollection();
+		}
+
+		private ProductInfoCollection()
+			:base("products")
+		{
+		}
+
+		public static ProductInfoCollection Instance
+		{
+			get { return instance; }
+		}
+	}
+}
