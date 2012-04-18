2133d4c09c2e002e34c7d0c20ffa1b2480c096c3
larry nung <larry.nung@waveface.com>
2012-04-18 12:17:33 +0800

    Add base interface and class for post latter
    feature

---
+++ Station/Wammer.Station.Service/Interface/IPostUploadSupportable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Wammer
+{
+	public interface IPostUploadSupportable
+	{
+		void AddPostUploadAction(PostUploadActionType actionType, string[] parameters);
+	}
+}
---
+++ Station/Wammer.Station.Service/Station/PostUploadTaskController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Wammer
+{
+	public class PostUploadTaskController : IPostUploadSupportable
+	{
+		public void AddPostUploadAction(PostUploadActionType actionType, string[] parameters)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
