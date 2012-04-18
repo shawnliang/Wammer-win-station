e52f54defbda1a4a78c842a8aeb695953520e4f5
larry nung <larry.nung@waveface.com>
2012-04-18 13:35:27 +0800

    Adjust AddPostUploadAction's parameter

--- Station/Wammer.Station.Service/Interface/IPostUploadSupportable.cs
+++ Station/Wammer.Station.Service/Interface/IPostUploadSupportable.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Collections.Specialized;
 
 namespace Wammer
 {
 	public interface IPostUploadSupportable
 	{
-		void AddPostUploadAction(PostUploadActionType actionType, string[] parameters);
+		void AddPostUploadAction(PostUploadActionType actionType, NameValueCollection parameters);
 	}
 }
--- Station/Wammer.Station.Service/Station/PostUploadTaskController.cs
+++ Station/Wammer.Station.Service/Station/PostUploadTaskController.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Collections.Specialized;
 
 namespace Wammer
 {
 	public class PostUploadTaskController : IPostUploadSupportable
 	{
-		public void AddPostUploadAction(PostUploadActionType actionType, string[] parameters)
+		public void AddPostUploadAction(PostUploadActionType actionType, NameValueCollection parameters)
 		{
 			throw new NotImplementedException();
 		}
