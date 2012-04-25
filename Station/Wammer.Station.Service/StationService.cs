54a619064c63ca7982de0ebf56e0a8bfba42fec5
Shawn Liang <shawn.liang@waveface.com>
2012-04-25 15:10:47 +0800

    suspend/resume syncing: can stop/restart upstreaming

--- Station/Wammer.Station.Service/StationService.cs
+++ Station/Wammer.Station.Service/StationService.cs
@@ -150,8 +150,8 @@ namespace Wammer.Station.Service
 
 		private void InitManagementServerHandler(AddDriverHandler addDriverHandler)
 		{
-			managementServer.AddHandler(GetDefaultBathPath("/station/resumeSync/"), new ResumeSyncHandler(postUploadRunner, stationTimer, bodySyncRunners));
-			managementServer.AddHandler(GetDefaultBathPath("/station/suspendSync/"), new SuspendSyncHandler(postUploadRunner, stationTimer, bodySyncRunners));
+			managementServer.AddHandler(GetDefaultBathPath("/station/resumeSync/"), new ResumeSyncHandler(postUploadRunner, stationTimer, bodySyncRunners, upstreamTaskRunner));
+			managementServer.AddHandler(GetDefaultBathPath("/station/suspendSync/"), new SuspendSyncHandler(postUploadRunner, stationTimer, bodySyncRunners, upstreamTaskRunner));
 			managementServer.AddHandler(GetDefaultBathPath("/station/drivers/add/"), addDriverHandler);
 			managementServer.AddHandler(GetDefaultBathPath("/station/drivers/list/"), new ListDriverHandler());
 			managementServer.AddHandler(GetDefaultBathPath("/station/drivers/remove/"), new RemoveOwnerHandler(stationId));
