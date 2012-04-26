abce52727cf1a7ffa452862f14e015334e95abb9
larry nung <larry.nung@waveface.com>
2012-04-26 13:58:01 +0800

    Fix SystemTrayIcon's display text

--- Station/StationSystemTray/MainForm.cs
+++ Station/StationSystemTray/MainForm.cs
@@ -116,6 +116,7 @@ namespace StationSystemTray
 			set
 			{
 				TrayIcon.Text = value;
+				TrayIcon.BalloonTipText = value;
 				TrayIcon.ShowBalloonTip(3, "Waveface", value, ToolTipIcon.None);
 			}
 		}
