85022b1951245721e002823861dc08ae73af10a1
larry nung <larry.nung@waveface.com>
2012-05-10 11:36:46 +0800

    Add SuppressIldasmAttribute

--- Client/Wammer/Properties/AssemblyInfo.cs
+++ Client/Wammer/Properties/AssemblyInfo.cs
@@ -1,9 +1,11 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+[assembly: SuppressIldasmAttribute()]
 [assembly: AssemblyTitle("Waveface Desktop")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
