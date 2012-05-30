1653f06cb72bccd6803e4adc67050dc5065c46ca
larry nung <larry.nung@waveface.com>
2012-05-30 17:44:39 +0800

    Add file to vcs

---
+++ Station/Wammer.Station.Service/Attribute/APIHandlerInfoAttribute.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Wammer
+{
+	public class APIHandlerInfoAttribute:Attribute
+	{
+		#region Property
+		/// <summary>
+		/// Gets or sets the type.
+		/// </summary>
+		/// <value>The type.</value>
+		public APIHandlerType Type { get; private set; }
+
+		/// <summary>
+		/// Gets or sets the path.
+		/// </summary>
+		/// <value>The API path.</value>
+		public string Path { get; private set; }
+		#endregion
+
+
+		#region Constructor
+		/// <summary>
+		/// Initializes a new instance of the <see cref="APIHandlerInfoAttribute"/> class.
+		/// </summary>
+		/// <param name="type">The type.</param>
+		/// <param name="path">The path.</param>
+		public APIHandlerInfoAttribute(APIHandlerType type, string path)
+		{
+			Type = type;
+			Path = path;
+		}
+		#endregion
+	}
+}
