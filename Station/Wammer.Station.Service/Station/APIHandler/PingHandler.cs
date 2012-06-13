da1ff4b5faf1a4c3ccf084fc1b4a1831c8fa5069
Shawn Liang <shawn.liang@waveface.com>
2012-06-13 15:35:00 +0800

    ping: verify user_id in ping handler
    devices bring user_id to station. station replies 400 if the user is not a
    valid driver of this station. This resolves the problem where device talks
    to someone else's station.

--- Station/Wammer.Station.Service/Station/APIHandler/PingHandler.cs
+++ Station/Wammer.Station.Service/Station/APIHandler/PingHandler.cs
@@ -1,11 +1,24 @@
-﻿namespace Wammer.Station
+﻿using MongoDB.Driver.Builders;
+using Wammer.Cloud;
+namespace Wammer.Station
 {
 	[APIHandlerInfo(APIHandlerType.Both, "/availability/ping/")]
 	internal class PingHandler : HttpHandler
 	{
 		public override void HandleRequest()
 		{
-			RespondSuccess();
+			var user_id = Parameters[CloudServer.PARAM_USER_ID];
+
+			// For backward compatibility, reply "success" even if remote does not provide user_id
+			if (string.IsNullOrEmpty(user_id))
+				RespondSuccess();
+
+			var user = Model.DriverCollection.Instance.FindOne(Query.EQ("_id", user_id));
+
+			if (user != null)
+				RespondSuccess();
+			else
+				RespondError("User not belonging to this station: " + user_id, (int)StationLocalApiError.InvalidDriver);
 		}
 
 		public override object Clone()
