60eb89fa96279038fa957ccbb8ff21c89ede57d3
larry nung <larry.nung@waveface.com>
2012-06-21 16:05:16 +0800

    Change square photo making process

--- Client/Wammer/Libs/Utility/ImageUtility.cs
+++ Client/Wammer/Libs/Utility/ImageUtility.cs
@@ -21,52 +21,53 @@ namespace Waveface
 
             try
             {
-                if ((image.Width >= size) || (image.Height >= size))
-                {
-                    double _prop = image.Width / (double)image.Height;
+				if ((image.Width < size) && (image.Height < size)) //圖片比指定大小還小，不需切割
+					return image;
 
-                    int _x;
-                    int _y;
+				//照比例縮放圖片
+				double _prop = image.Width / (double)image.Height;
 
-                    if (image.Width > image.Height)
-                    {
-                        _x = (int)Math.Round(size * _prop, 0);
-                        _y = size;
-                    }
-                    else
-                    {
-                        _x = size;
-                        _y = (int)Math.Round(size / _prop, 0);
-                    }
+				int _x;
+				int _y;
 
-                    using (Image _img = new Bitmap(image, new Size(_x, _y)))
-                    {
-                        Image _crapped = new Bitmap(size, size);
-                        Graphics _g = Graphics.FromImage(_crapped);
+				var isHorizontalPhoto = image.Width > image.Height;
+				if (isHorizontalPhoto)
+				{
+					_x = (int)Math.Round(size * _prop, 0);
+					_y = size;
+				}
+				else
+				{
+					_x = size;
+					_y = (int)Math.Round(size / _prop, 0);
+				}
 
-                        if (image.Width > image.Height)
-                        {
-                            _g.DrawImage(_img,
-                                         new Rectangle(0, 0, size, size),
-                                         new Rectangle((image.Width - image.Height) / 2, 0, size, size),
-                                         GraphicsUnit.Pixel
-                                );
-                        }
-                        else
-                        {
-                            _g.DrawImage(_img,
-                                         new Rectangle(0, 0, size, size),
-                                         new Rectangle(0, (image.Height - image.Width) / 2, size, size),
-                                         GraphicsUnit.Pixel
-                                );
-                        }
+				//切方圖
+				using (Image _img = new Bitmap(image, new Size(_x, _y)))
+				{
+					Image _crapped = new Bitmap(size, size);
+					Graphics _g = Graphics.FromImage(_crapped);
 
-                        return _crapped;
-                    }
-                }
+					if (isHorizontalPhoto)
+					{
+						_g.DrawImage(_img,
+									 new Rectangle(0, 0, size, size),
+									 new Rectangle((image.Width - image.Height) / 2, 0, size, size),
+									 GraphicsUnit.Pixel
+							);
+					}
+					else
+					{
+						_g.DrawImage(_img,
+									 new Rectangle(0, 0, size, size),
+									 new Rectangle(0, (int)(image.Height * 0.08), size, size),
+									 GraphicsUnit.Pixel
+							);
+					}
 
-                return image;
-            }
+					return _crapped;
+				}
+			}
             catch
             {
                 return image;
