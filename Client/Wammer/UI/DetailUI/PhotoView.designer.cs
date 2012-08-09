aff6311e4eb37f046b1b3276ada71db56361163d
larry nung <larry.nung@waveface.com>
2012-08-09 15:10:26 +0800

    Fix Bug: Gallery view can't fullscreen in some dpi or pc

--- Client/Wammer/UI/DetailUI/PhotoView.designer.cs
+++ Client/Wammer/UI/DetailUI/PhotoView.designer.cs
@@ -45,8 +45,8 @@ namespace Waveface.DetailUI
 			this.thumbnailNavigator1 = new Waveface.PhotoNavigator();
 			this.panelBottom = new System.Windows.Forms.Panel();
 			this.pnlPhotoViewToolBar = new System.Windows.Forms.Panel();
-			this.cultureManager = new Waveface.Localization.CultureManager(this.components);
 			this.imageBox = new Waveface.Component.ImageBox();
+			this.cultureManager = new Waveface.Localization.CultureManager(this.components);
 			this.contextMenuStrip.SuspendLayout();
 			this.panelBottom.SuspendLayout();
 			this.pnlPhotoViewToolBar.SuspendLayout();
@@ -54,39 +54,34 @@ namespace Waveface.DetailUI
 			// 
 			// contextMenuStrip
 			// 
-			resources.ApplyResources(this.contextMenuStrip, "contextMenuStrip");
 			this.contextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.miSave,
             this.miSetAsCoverImage});
 			this.contextMenuStrip.Name = "contextMenuStrip";
-			this.toolTip.SetToolTip(this.contextMenuStrip, resources.GetString("contextMenuStrip.ToolTip"));
+			resources.ApplyResources(this.contextMenuStrip, "contextMenuStrip");
 			// 
 			// miSave
 			// 
-			resources.ApplyResources(this.miSave, "miSave");
 			this.miSave.Image = global::Waveface.Properties.Resources.FB_save;
 			this.miSave.Name = "miSave";
+			resources.ApplyResources(this.miSave, "miSave");
 			this.miSave.Click += new System.EventHandler(this.miSave_Click);
 			// 
 			// miSetAsCoverImage
 			// 
-			resources.ApplyResources(this.miSetAsCoverImage, "miSetAsCoverImage");
 			this.miSetAsCoverImage.Image = global::Waveface.Properties.Resources.FB_cover;
 			this.miSetAsCoverImage.Name = "miSetAsCoverImage";
+			resources.ApplyResources(this.miSetAsCoverImage, "miSetAsCoverImage");
 			this.miSetAsCoverImage.Click += new System.EventHandler(this.miSetAsCoverImage_Click);
-			// 
-			// saveFileDialog
-			// 
-			resources.ApplyResources(this.saveFileDialog, "saveFileDialog");
 			// 
 			// imageButton1
 			// 
-			resources.ApplyResources(this.imageButton1, "imageButton1");
 			this.imageButton1.CenterAlignImage = false;
 			this.imageButton1.Image = global::Waveface.Properties.Resources.back;
 			this.imageButton1.ImageDisable = global::Waveface.Properties.Resources.back;
 			this.imageButton1.ImageFront = null;
 			this.imageButton1.ImageHover = global::Waveface.Properties.Resources.back_hl;
+			resources.ApplyResources(this.imageButton1, "imageButton1");
 			this.imageButton1.Name = "imageButton1";
 			this.imageButton1.TextShadow = true;
 			this.toolTip.SetToolTip(this.imageButton1, resources.GetString("imageButton1.ToolTip"));
@@ -94,12 +89,12 @@ namespace Waveface.DetailUI
 			// 
 			// btnSave
 			// 
-			resources.ApplyResources(this.btnSave, "btnSave");
 			this.btnSave.CenterAlignImage = false;
 			this.btnSave.Image = global::Waveface.Properties.Resources.Save;
 			this.btnSave.ImageDisable = global::Waveface.Properties.Resources.FB_save_hl;
 			this.btnSave.ImageFront = null;
 			this.btnSave.ImageHover = global::Waveface.Properties.Resources.FB_save_hl;
+			resources.ApplyResources(this.btnSave, "btnSave");
 			this.btnSave.Name = "btnSave";
 			this.btnSave.TextShadow = true;
 			this.toolTip.SetToolTip(this.btnSave, resources.GetString("btnSave.ToolTip"));
@@ -107,12 +102,12 @@ namespace Waveface.DetailUI
 			// 
 			// btnCoverImage
 			// 
-			resources.ApplyResources(this.btnCoverImage, "btnCoverImage");
 			this.btnCoverImage.CenterAlignImage = false;
 			this.btnCoverImage.Image = global::Waveface.Properties.Resources.cover;
 			this.btnCoverImage.ImageDisable = global::Waveface.Properties.Resources.cover;
 			this.btnCoverImage.ImageFront = null;
 			this.btnCoverImage.ImageHover = global::Waveface.Properties.Resources.cover_hl;
+			resources.ApplyResources(this.btnCoverImage, "btnCoverImage");
 			this.btnCoverImage.Name = "btnCoverImage";
 			this.btnCoverImage.TextShadow = true;
 			this.toolTip.SetToolTip(this.btnCoverImage, resources.GetString("btnCoverImage.ToolTip"));
@@ -120,12 +115,12 @@ namespace Waveface.DetailUI
 			// 
 			// btnSlideShow
 			// 
-			resources.ApplyResources(this.btnSlideShow, "btnSlideShow");
 			this.btnSlideShow.CenterAlignImage = false;
 			this.btnSlideShow.Image = global::Waveface.Properties.Resources.slide;
 			this.btnSlideShow.ImageDisable = global::Waveface.Properties.Resources.slide;
 			this.btnSlideShow.ImageFront = null;
 			this.btnSlideShow.ImageHover = global::Waveface.Properties.Resources.slide_hl;
+			resources.ApplyResources(this.btnSlideShow, "btnSlideShow");
 			this.btnSlideShow.Name = "btnSlideShow";
 			this.btnSlideShow.TextShadow = true;
 			this.toolTip.SetToolTip(this.btnSlideShow, resources.GetString("btnSlideShow.ToolTip"));
@@ -133,22 +128,20 @@ namespace Waveface.DetailUI
 			// 
 			// thumbnailNavigator1
 			// 
-			resources.ApplyResources(this.thumbnailNavigator1, "thumbnailNavigator1");
 			this.thumbnailNavigator1.BackColor = System.Drawing.Color.Black;
 			this.thumbnailNavigator1.DefaultThumbnail = ((System.Drawing.Image)(resources.GetObject("thumbnailNavigator1.DefaultThumbnail")));
+			resources.ApplyResources(this.thumbnailNavigator1, "thumbnailNavigator1");
 			this.thumbnailNavigator1.Name = "thumbnailNavigator1";
 			this.thumbnailNavigator1.SelectedIndex = -1;
 			this.thumbnailNavigator1.ThumbnailPadding = new System.Windows.Forms.Padding(0);
 			this.thumbnailNavigator1.ThumbnailWidth = 128;
-			this.toolTip.SetToolTip(this.thumbnailNavigator1, resources.GetString("thumbnailNavigator1.ToolTip"));
 			// 
 			// panelBottom
 			// 
-			resources.ApplyResources(this.panelBottom, "panelBottom");
 			this.panelBottom.BackColor = System.Drawing.Color.Black;
 			this.panelBottom.Controls.Add(this.pnlPhotoViewToolBar);
+			resources.ApplyResources(this.panelBottom, "panelBottom");
 			this.panelBottom.Name = "panelBottom";
-			this.toolTip.SetToolTip(this.panelBottom, resources.GetString("panelBottom.ToolTip"));
 			// 
 			// pnlPhotoViewToolBar
 			// 
@@ -158,24 +151,22 @@ namespace Waveface.DetailUI
 			this.pnlPhotoViewToolBar.Controls.Add(this.btnCoverImage);
 			this.pnlPhotoViewToolBar.Controls.Add(this.btnSlideShow);
 			this.pnlPhotoViewToolBar.Name = "pnlPhotoViewToolBar";
-			this.toolTip.SetToolTip(this.pnlPhotoViewToolBar, resources.GetString("pnlPhotoViewToolBar.ToolTip"));
-			// 
-			// cultureManager
-			// 
-			this.cultureManager.ManagedControl = this;
 			// 
 			// imageBox
 			// 
-			resources.ApplyResources(this.imageBox, "imageBox");
 			this.imageBox.AutoPan = false;
+			resources.ApplyResources(this.imageBox, "imageBox");
 			this.imageBox.BackColor = System.Drawing.Color.Black;
 			this.imageBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
 			this.imageBox.GridColor = System.Drawing.Color.Black;
 			this.imageBox.GridColorAlternate = System.Drawing.Color.Black;
 			this.imageBox.Name = "imageBox";
-			this.toolTip.SetToolTip(this.imageBox, resources.GetString("imageBox.ToolTip"));
 			this.imageBox.ZoomIncrement = 10;
 			this.imageBox.Click += new System.EventHandler(this.imageBox_Click);
+			// 
+			// cultureManager
+			// 
+			this.cultureManager.ManagedControl = this;
 			// 
 			// PhotoView
 			// 
@@ -188,7 +179,7 @@ namespace Waveface.DetailUI
 			this.MinimizeBox = false;
 			this.Name = "PhotoView";
 			this.ShowInTaskbar = false;
-			this.toolTip.SetToolTip(this, resources.GetString("$this.ToolTip"));
+			this.Load += new System.EventHandler(this.PhotoView_Load);
 			this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.PhotoView_KeyDown);
 			this.Resize += new System.EventHandler(this.PhotoView_Resize);
 			this.contextMenuStrip.ResumeLayout(false);
