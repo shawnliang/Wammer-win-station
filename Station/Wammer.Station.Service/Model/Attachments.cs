cffe6eb5030e82a4a6f610903a85b22cb1ba0f8d
Shawn Liang <shawn.liang@waveface.com>
2012-10-09 12:11:37 +0800

    fix bug: gps structure is not serializable

--- Station/Wammer.Station.Service/Model/Attachments.cs
+++ Station/Wammer.Station.Service/Model/Attachments.cs
@@ -215,6 +215,7 @@ namespace Wammer.Model
 	}
 
 	[DataContract]
+	[Serializable]
 	public class GPSInfo
 	{
 		[DataMember(Name = "1")]
@@ -230,6 +231,7 @@ namespace Wammer.Model
 		public List<List<int>> GPSLongitude { get; set; }
 	}
 
+	[Serializable]
 	public class Gps
 	{
 		public double longitude { get; set; }
