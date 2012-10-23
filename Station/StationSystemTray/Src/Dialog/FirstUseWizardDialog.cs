c96827bebca284462dcd41343444709a69ec29f4
larry nung <larry.nung@waveface.com>
2012-10-23 11:47:10 +0800

    Add WizardControl to process wizard flow Add FirstUseWizardDialog

---
+++ Station/StationSystemTray/Src/Dialog/FirstUseWizardDialog.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using StationSystemTray.Src.Control;
+
+namespace StationSystemTray
+{
+	public partial class FirstUseWizardDialog : Form
+	{
+		#region Private Property
+		private string m_OriginalTitle { get; set; } 
+		#endregion
+
+
+		#region Constructor
+		public FirstUseWizardDialog()
+		{
+			InitializeComponent();
+
+			m_OriginalTitle = this.Text;
+
+			wizardControl1.SetWizardPages(new Control[]
+			{
+				new BuildPersonalCloudUserControl()
+			});
+		} 
+		#endregion
+
+
+		#region Event Process
+		private void FirstUseWizardDialog_Load(object sender, EventArgs e)
+		{
+			this.Text = string.Format("{0} ({1} of {2})", m_OriginalTitle, wizardControl1.PageIndex, wizardControl1.PageCount);
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			wizardControl1.NextPage();
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			wizardControl1.PreviousPage();
+		} 
+		#endregion
+	}
+}
