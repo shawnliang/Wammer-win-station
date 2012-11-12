11ea27134ed119dd62fc71b90ec78d23e8326d4a
Shawn Liang <shawn.liang@waveface.com>
2012-11-12 12:03:38 +0800

    upload metadata: adjust ui

--- Station/StationSystemTray/Src/Control/PersonalCloudStatusControl.Designer.cs
+++ Station/StationSystemTray/Src/Control/PersonalCloudStatusControl.Designer.cs
@@ -53,7 +53,7 @@
 			// 
 			this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
 			this.button1.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.button1.Location = new System.Drawing.Point(32, 325);
+			this.button1.Location = new System.Drawing.Point(30, 325);
 			this.button1.Name = "button1";
 			this.button1.Size = new System.Drawing.Size(240, 27);
 			this.button1.TabIndex = 2;
