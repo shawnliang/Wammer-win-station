d203134111588796f1d3a58702bf3885010ac7b6
Shawn Liang <shawn.liang@waveface.com>
2012-11-13 21:26:55 +0800

    refactor: remove unused code

--- Station/StationSystemTray/Src/Class/PhotoSearch.cs
+++ Station/StationSystemTray/Src/Class/PhotoSearch.cs
@@ -22,36 +22,22 @@ namespace StationSystemTray
 
 		private HashSet<string> ignorePath = new HashSet<string>();
 		private HashSet<PathAndPhotoCount> _interestedPaths;
-		private string _picasaDBStoragePath;
-		private string _albumPathPMPFileName;
 		private Dictionary<string, int> m_InterestedFileCountInPhotos = new Dictionary<string, int>();
 		private BackgroundWorker backgroundWorker1;
 
+		public event EventHandler<MetadataUploadEventArgs> MetadataUploaded;
 
-		/// <summary>
-		/// Gets the m_ picasa DB storage path.
-		/// </summary>
-		/// <value>The m_ picasa DB storage path.</value>
-		private string m_PicasaDBStoragePath
-		{
-			get
-			{
-				return _picasaDBStoragePath ??
-					(_picasaDBStoragePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), PICASA_DB_RELATIVED_STORAGE_PATH));
-			}
-		}
+		public event EventHandler<FileImportedEventArgs> FileImported;
 
-		/// <summary>
-		/// Gets the name of the m_ album path PMP file.
-		/// </summary>
-		/// <value>The name of the m_ album path PMP file.</value>
-		private string m_AlbumPathPMPFileName
+		public event EventHandler<Wammer.Station.ImportDoneEventArgs> ImportDone;
+
+
+		public PhotoSearch()
 		{
-			get
-			{
-				return _albumPathPMPFileName ??
-					(_albumPathPMPFileName = Path.Combine(m_PicasaDBStoragePath, ALBUM_PATH_PMP_FILENAME));
-			}
+			ignorePath.Add(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles));
+			ignorePath.Add(Environment.GetFolderPath(Environment.SpecialFolder.System));
+			ignorePath.Add(Environment.GetFolderPath(Environment.SpecialFolder.InternetCache));
+			ignorePath.Add(Environment.GetFolderPath(Environment.SpecialFolder.CommonProgramFiles));
 		}
 
 
@@ -72,23 +58,6 @@ namespace StationSystemTray
 			}
 		}
 
-		/// <summary>
-		/// Gets or sets the m_ interested extensions.
-		/// </summary>
-		/// <value>The m_ interested extensions.</value>
-		private HashSet<String> m_InterestedExtensions { get; set; }
-
-
-		public PhotoSearch()
-		{
-			m_InterestedExtensions = new HashSet<String> { ".jpg", ".jpeg", ".bmp", ".png" };
-			ignorePath.Add(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles));
-			ignorePath.Add(Environment.GetFolderPath(Environment.SpecialFolder.System));
-			ignorePath.Add(Environment.GetFolderPath(Environment.SpecialFolder.InternetCache));
-			ignorePath.Add(Environment.GetFolderPath(Environment.SpecialFolder.CommonProgramFiles));
-		}
-
-
 		public void StartSearchAsync()
 		{
 			backgroundWorker1 = new BackgroundWorker();
@@ -96,11 +65,6 @@ namespace StationSystemTray
 			backgroundWorker1.RunWorkerAsync();
 		}
 
-		/// <summary>
-		/// Handles the DoWork event of the backgroundWorker1 control.
-		/// </summary>
-		/// <param name="sender">The source of the event.</param>
-		/// <param name="e">The <see cref="System.ComponentModel.DoWorkEventArgs"/> instance containing the event data.</param>
 		private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
 		{
 			Thread.CurrentThread.Priority = ThreadPriority.Lowest;
@@ -157,107 +121,21 @@ namespace StationSystemTray
 
 		private static int JpgFileCount(string path)
 		{
+			var total = 0;
+
 			var jpg = Directory.GetFiles(path, "*.jpg");
 			if (jpg != null && jpg.Any(HasDateTimeInExif))
 			{
-				return jpg.Length;
+				total += jpg.Length;
 			}
 
 			var jpeg = Directory.GetFiles(path, "*.jpeg");
 			if (jpeg != null && jpeg.Any(HasDateTimeInExif))
-				return jpg.Length;
-
-			return 0;
-		}
-
-		/// <summary>
-		/// Determines whether [is valid picasa format] [the specified file].
-		/// </summary>
-		/// <param name="file">The file.</param>
-		/// <returns>
-		/// 	<c>true</c> if [is valid picasa format] [the specified file]; otherwise, <c>false</c>.
-		/// </returns>
-		private Boolean IsValidPicasaFormat(string file)
-		{
-			using (var fs = File.OpenRead(file))
 			{
-				using (var br = new BinaryReader(fs))
-				{
-					return IsValidPicasaFormat(br);
-				}
+				total += jpeg.Length;
 			}
-		}
 
-
-		/// <summary>
-		/// Determines whether [is valid picasa format] [the specified br].
-		/// </summary>
-		/// <param name="br">The br.</param>
-		/// <returns>
-		/// 	<c>true</c> if [is valid picasa format] [the specified br]; otherwise, <c>false</c>.
-		/// </returns>
-		private Boolean IsValidPicasaFormat(BinaryReader br)
-		{
-			if (br == null)
-				throw new ArgumentNullException("br");
-
-			var position = br.BaseStream.Position;
-			try
-			{
-				br.BaseStream.Seek(0, SeekOrigin.Begin);
-				var magic = br.ReadBytes(4);
-				if (magic[0] != 0xcd ||
-					magic[1] != 0xcc ||
-					magic[2] != 0xcc ||
-					magic[3] != 0x3f)
-				{
-					return false;
-				}
-
-				var type = br.ReadInt16();
-
-				if (0x1332 != br.ReadInt16())
-				{
-					return false;
-				}
-
-				if (0x00000002 != br.ReadInt32())
-				{
-					return false;
-				}
-
-				if (type != br.ReadInt16())
-				{
-					return false;
-				}
-
-				if (0x1332 != br.ReadInt16())
-				{
-					return false;
-				}
-				return true;
-			}
-			finally
-			{
-				br.BaseStream.Seek(position, SeekOrigin.Begin);
-			}
-		}
-
-		private long GetFolderSize(string folder)
-		{
-			try
-			{
-				Type tp = Type.GetTypeFromProgID("Scripting.FileSystemObject");
-				object fso = Activator.CreateInstance(tp);
-				object fd = tp.InvokeMember("GetFolder", BindingFlags.InvokeMethod, null, fso, new object[] { folder });
-				long ret = Convert.ToInt64(tp.InvokeMember("Size", BindingFlags.GetProperty, null, fd, null));
-				Marshal.ReleaseComObject(fso);
-				return ret;
-			}
-			catch (Exception)
-			{
-				return 0;
-			}
+			return total;
 		}
 
 		private void AddInterestedPath(String path, int photoCount)
@@ -336,96 +214,10 @@ namespace StationSystemTray
 			m_InterestedPaths.Add(new PathAndPhotoCount(path, photoCount));
 		}
 
-		/// <summary>
-		/// Checks the picasa format.
-		/// </summary>
-		/// <param name="file">The file.</param>
-		private void CheckPicasaFormat(string file)
-		{
-			using (var fs = File.OpenRead(file))
-			{
-				using (var br = new BinaryReader(fs))
-				{
-					CheckPicasaFormat(br);
-				}
-			}
-		}
-
-		/// <summary>
-		/// Checks the picasa format.
-		/// </summary>
-		/// <param name="br">The br.</param>
-		private void CheckPicasaFormat(BinaryReader br)
-		{
-			if (!IsValidPicasaFormat(br))
-				throw new FileFormatException("Incorrect picasa file format.");
-		}
-
-
-
-
-		/// <summary>
-		/// Reads all string field.
-		/// </summary>
-		/// <param name="file">The file.</param>
-		/// <returns></returns>
-		private IEnumerable<string> ReadAllStringField(string file)
-		{
-			using (var fs = File.OpenRead(file))
-			{
-				using (var br = new BinaryReader(fs))
-				{
-					br.BaseStream.Seek(16, SeekOrigin.Begin);
-					var number = br.ReadInt32();
-
-					for (long i = 0; i < number; i++)
-					{
-						yield return getString(br);
-					}
-					yield break;
-				}
-			}
-		}
-
-		/// <summary>
-		/// Reads all string field.
-		/// </summary>
-		/// <param name="br">The br.</param>
-		/// <returns></returns>
-		private IEnumerable<string> ReadAllStringField(BinaryReader br)
-		{
-			br.BaseStream.Seek(16, SeekOrigin.Begin);
-			var number = br.ReadInt32();
-
-			for (long i = 0; i < number; i++)
-			{
-				yield return getString(br);
-			}
-			yield break;
-		}
-
-		/// <summary>
-		/// Gets the string.
-		/// </summary>
-		/// <param name="br">The br.</param>
-		/// <returns></returns>
-		private String getString(BinaryReader br)
-		{
-			var sb = new StringBuilder();
-			int c;
-			while ((c = br.Read()) != 0)
-			{
-				sb.Append((char)c);
-			}
-			return sb.ToString();
-		}
-
 		public void ImportToStationAsync(IEnumerable<string> paths, string session_token)
 		{
 			var loginedSession = LoginedSessionCollection.Instance.FindOne(Query.EQ("_id", session_token));
 			var groupID = loginedSession.groups.First().group_id;
-			//StationAPI.Import(session_token, groupID, string.Format("[{0}]", string.Join(",", paths.ToArray())));
-
 			var transaction = new Wammer.Station.Management.ImportTranscation(
 				loginedSession.user.user_id, session_token, StationAPI.API_KEY, paths);
 
@@ -456,11 +248,5 @@ namespace StationSystemTray
 			if (handler != null)
 				handler(this, e);
 		}
-
-		public event EventHandler<MetadataUploadEventArgs> MetadataUploaded;
-
-		public event EventHandler<FileImportedEventArgs> FileImported;
-
-		public event EventHandler<Wammer.Station.ImportDoneEventArgs> ImportDone;
 	}
 }
