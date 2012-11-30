af90b93795100a7e37e62c78c94725042b357ef7
Shawn Liang <shawn.liang@waveface.com>
2012-11-30 12:16:56 +0800

    fix publish failure

--- Tools/publishBin.py
+++ Tools/publishBin.py
@@ -4,25 +4,20 @@ import glob
 import shutil
 from ftplib import FTP
 
-solution_dir = os.path.dirname(os.path.abspath(__file__))
-Wammer_release_dir = os.path.join(solution_dir, "Client/Wammer/bin/Release")
-StationService_release_dir = os.path.join(solution_dir, "Station/Wammer.Station.Service/bin/Release")
-StationSystemTray_release_dir = os.path.join(solution_dir, "Station/StationSystemTray/bin/Release")
+solution_dir = sys.argv[2]
+Wammer_release_dir = os.path.join(solution_dir, "Bin/Release")
 
 if __name__ == "__main__":
     version = sys.argv[1]
     os.mkdir(version)
     for pdb in glob.glob("{0}/*.pdb".format(Wammer_release_dir)):
         shutil.copy2(pdb, version)
-    for pdb in glob.glob("{0}/*.pdb".format(StationSystemTray_release_dir)):
-        shutil.copy2(pdb, version)
-    for pdb in glob.glob("{0}/*.pdb".format(StationService_release_dir)):
-        shutil.copy2(pdb, version)
-    shutil.copy2('{0}/development-WavefaceSetup-{1}.exe'.format(os.path.dirname(solution_dir), version), version)
+
+	shutil.copy2('{0}/development-WavefaceSetup-{1}.exe'.format(os.path.dirname(solution_dir), version), version)
     shutil.copy2('{0}/production-WavefaceSetup-{1}.exe'.format(os.path.dirname(solution_dir), version), version)
     shutil.copy2('{0}/staging-WavefaceSetup-{1}.exe'.format(os.path.dirname(solution_dir), version), version)
-    shutil.copy2('{0}/versioninfo.xml'.format(os.path.dirname(solution_dir)), version)
-    shutil.copy2('{0}/versioninfo_dev.xml'.format(os.path.dirname(solution_dir)), version)
+    #shutil.copy2('{0}/versioninfo.xml'.format(os.path.dirname(solution_dir)), version)
+    #shutil.copy2('{0}/versioninfo_dev.xml'.format(os.path.dirname(solution_dir)), version)
     ftp = FTP("WF-NAS", "admin", "13579246801")
     ftp.mkd("Users/WavefaceStation/Builds/{0}".format(version))
     ftp.cwd("Users/WavefaceStation/Builds/{0}".format(version))
