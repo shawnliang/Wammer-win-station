623fd3a2efa407445474eebd8b1c1f51a57efdd5
Shawn Liang <shawn.liang@waveface.com>
2012-12-01 22:58:41 +0800

    doc: add api handler to process monitor/add request

---
+++ Source/Wammer.Station.Service/Station/APIHandler/MonitorAddHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Wammer.Station.APIHandler
+{
+	[APIHandlerInfo(APIHandlerType.ManagementAPI, "/station/monitor/add")]
+	public class MonitorAddHandler : HttpHandler
+	{
+		private Wammer.Station.Doc.MonitorAddHandlerImp imp = new Doc.MonitorAddHandlerImp();
+
+		#region Public Method
+		/// <summary>
+		/// Handles the request.
+		/// </summary>
+		public override void HandleRequest()
+		{
+			CheckParameter("apikey", "session_token", "user_id", "file");
+
+			imp.Process(Parameters["apikey"], Parameters["session_token"], Parameters["user_id"], Parameters["file"]);
+			RespondSuccess();
+		}
+
+		/// <summary>
+		/// Clones this instance.
+		/// </summary>
+		/// <returns></returns>
+		public override object Clone()
+		{
+			return this.MemberwiseClone();
+		}
+		#endregion
+	}
+}
---
+++ Source/Wammer.Station.Service/Station/Doc/MonitorAddHandlerImp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Wammer.Station.Doc
+{
+	public class MonitorAddHandlerImp
+	{
+		public void Process(string apikey, string session_token, string user_id, string file)
+		{
+
+		}
+	}
+}
