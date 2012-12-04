ea2c0c56787cfddd28f7315e82671d86e5f5d835
larrynung <larrynung@hotmail.com>
2012-12-04 15:54:41 +0800

    Implement station collections function

---
+++ Source/Wammer.Station.Service/Model/Collection/CollectionCollection.cs
@@ -0,0 +1,38 @@
+﻿namespace Wammer.Model
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public class CollectionCollection : Collection<Collection>
+	{
+		#region Static Var
+
+		private static CollectionCollection instance;
+
+		#endregion
+
+		#region Constructor
+		/// <summary>
+		/// Prevents a default instance of the <see cref="CollectionCollection" /> class from being created.
+		/// </summary>
+		private CollectionCollection()
+			: base("Collection")
+		{
+		}
+
+		#endregion
+
+		#region Public Static Method
+
+		/// <summary>
+		/// Gets the instance.
+		/// </summary>
+		/// <value>The instance.</value>
+		public static CollectionCollection Instance
+		{
+			get { return instance ?? (instance = new CollectionCollection()); }
+		}
+
+		#endregion
+	}
+}
