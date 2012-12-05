fa4fce314d679a85bb0f2b3ecad13ccfb426410c
larrynung <larrynung@hotmail.com>
2012-12-05 17:35:54 +0800

    Implement unSubscribeEvent API

---
+++ Source/Waveface.Stream.ClientFramework/Src/Class/WebSocketCommand/UnSubscribeEventCommand.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WebSocketSharp.Server;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace Waveface.Stream.ClientFramework
+{
+    [Obfuscation]
+    public class SubscribeEventCommand : WebSocketCommandBase
+	{
+		#region Public Property
+		/// <summary>
+		/// Gets the name.
+		/// </summary>
+		/// <value>The name.</value>
+		public override string Name
+		{
+            get { return "unSubscribeEvent"; }
+		}
+		#endregion
+
+
+		#region Public Method
+        /// <summary>
+        /// Executes the specified parameters.
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public override Dictionary<string, Object> Execute(WebSocketCommandData data)
+		{
+            var parameters = data.Parameters;
+
+            var eventID = parameters.ContainsKey("event_id") ? int.Parse(parameters["event_id"].ToString()) : 0;
+            var systemEvent = (SystemEventType)eventID;
+
+            var subscribedEvents = StreamClient.Instance.LoginedUser.SubscribedEvents;
+
+            if (systemEvent == SystemEventType.All)
+            {
+                data.Parameters.Clear();
+                foreach (SystemEventType systemEventType in Enum.GetValues(typeof(SystemEventType)))
+                {
+                    if (subscribedEvents.ContainsKey(systemEventType))
+                        subscribedEvents.Remove(systemEventType);
+                }
+                return null;
+            }
+
+            if (subscribedEvents.ContainsKey(systemEvent))
+                subscribedEvents.Remove(systemEvent);
+
+            return null;
+		}
+		#endregion
+	}
+}
