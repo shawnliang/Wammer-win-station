76f753007a4708d1d0a7f6ac4f1c58509b945cc6
larrynung <larrynung@hotmail.com>
2012-12-26 17:37:47 +0800

    update new client's web page

--- Source/Waveface.Stream.WindowsClient/Web/javascripts/com/gmap.js
+++ Source/Waveface.Stream.WindowsClient/Web/javascripts/com/gmap.js
@@ -1,10 +1,1 @@
-(function() {
-
-  define(['async!https://maps.googleapis.com/maps/api/js?key=AIzaSyCAo25juPfKZGZ08gdgVPr_ZKSPNjxA6aw&sensor=false'], function(gmaps) {
-    if (window.navigator.online) {
-      return window.google.maps;
-    }
-    return {};
-  });
-
-}).call(this);
+(function(){define(["async!https://maps.googleapis.com/maps/api/js?key=AIzaSyCAo25juPfKZGZ08gdgVPr_ZKSPNjxA6aw&sensor=false"],function(e){return window.navigator.online?window.google.maps:{}})}).call(this);
