f53a94a667ec856ac514e169d3a550267793a1ef
Shawn Liang <shawn.liang@waveface.com>
2012-12-27 10:07:33 +0800

    client: add import menu item to sys tray menu

---
+++ Source/Waveface.Stream.WindowsClient/Src/Dialog/FileImportDialog.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Waveface.Stream.ClientFramework;
+
+namespace Waveface.Stream.WindowsClient
+{
+	public partial class FileImportDialog : StepByStepWizardDialog
+	{
+		private static FileImportDialog _instance;
+
+		public static FileImportDialog Instance
+		{
+			get
+			{
+				return (_instance == null || _instance.IsDisposed) ? (_instance = new FileImportDialog()) : _instance;
+			}
+		}
+
+		private FileImportDialog()
+		{
+			InitializeComponent();
+
+			if (!StreamClient.Instance.IsLogined)
+				throw new InvalidOperationException();
+
+			wizardControl.Parameters.Set("user_id", StreamClient.Instance.LoginedUser.UserID);
+			wizardControl.Parameters.Set("session_token", StreamClient.Instance.LoginedUser.SessionToken);
+
+			wizardControl.SetWizardPages(new StepPageControl[]
+			{
+				new FileImportControl()
+			});
+		}
+	}
+}
