2800892c45c46f093da4f4bf6f12e4a8994f8eca
Shawn Liang <shawn.liang@waveface.com>
2013-01-16 09:39:25 +0800

    import: update import task's UploadCount when task is aborted

--- Source/Wammer.Station.Service/Station/AttachmentUpload/UpstreamTask.cs
+++ Source/Wammer.Station.Service/Station/AttachmentUpload/UpstreamTask.cs
@@ -57,7 +57,10 @@ namespace Wammer.Station.AttachmentUpload
 				user_id = user.user_id;
 
 				if (meta == ImageMeta.Origin && !user.isPaidUser)
+				{
+					updateImportTaskIfNeeded(0);
 					return;
+				}
 
 				IAttachmentInfo info = attachment.GetInfoByMeta(meta);
 				if (info == null)
@@ -96,19 +99,17 @@ namespace Wammer.Station.AttachmentUpload
 
 				DriverCollection.Instance.Update(Query.EQ("_id", user_id), Update.Unset("sync_range.upload_error"));
 
-				if (importTaskId.HasValue)
-				{
-					TaskStatusCollection.Instance.Update(Query.EQ("_id", importTaskId.Value),
-						Update.Inc("UploadedSize", uploadedSize).Inc("UploadedCount", 1));
-				}
+				updateImportTaskIfNeeded(uploadedSize);
 			}
 			catch (WammerCloudException e)
 			{
 				if (e.WammerError == (int)AttachmentApiError.FileExisted ||
 					e.WammerError == (int)AttachmentApiError.InvalidObjectId ||
-					e.WammerError == (int)AttachmentApiError.InvalidPostId)
+					e.WammerError == (int)AttachmentApiError.InvalidPostId ||
+					e.WammerError == (int)AttachmentApiError.InvalidImage)
 				{
 					this.LogWarnMsg("attachment " + object_id + " is rejected by Cloud. Drop this task.");
+					updateImportTaskIfNeeded(0);
 				}
 				else
 				{
@@ -131,6 +132,15 @@ namespace Wammer.Station.AttachmentUpload
 			finally
 			{
 				upstreamCount.Decrement();
+			}
+		}
+
+		private void updateImportTaskIfNeeded(long uploadedSize)
+		{
+			if (importTaskId.HasValue)
+			{
+				TaskStatusCollection.Instance.Update(Query.EQ("_id", importTaskId.Value),
+					Update.Inc("UploadedSize", uploadedSize).Inc("UploadedCount", 1));
 			}
 		}
 
