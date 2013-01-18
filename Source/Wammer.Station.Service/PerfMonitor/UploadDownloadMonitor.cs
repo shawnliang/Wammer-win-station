a6185d8e31bac7cc0514c4c99c455adbd8f89a31
Shawn Liang <shawn.liang@waveface.com>
2013-01-18 14:09:06 +0800

    status: show megabytes to download instead of file counts

--- Source/Wammer.Station.Service/PerfMonitor/UploadDownloadMonitor.cs
+++ Source/Wammer.Station.Service/PerfMonitor/UploadDownloadMonitor.cs
@@ -23,6 +23,8 @@ namespace Wammer.PerfMonitor
 		private readonly IPerfCounter UpstreamNumCounter;
 		private readonly IPerfCounter UpstreamRateCounter;
 		private readonly IPerfCounter attachmentUploadCounter;
+		private readonly IPerfCounter bytesToDownloadCounter;
+		private readonly IPerfCounter bytesToUploadCounter;
 
 		private static UploadDownloadMonitor instance;
 
@@ -43,6 +45,9 @@ namespace Wammer.PerfMonitor
 			UpstreamRateCounter = PerfCounter.GetCounter(PerfCounter.UPSTREAM_RATE);
 
 			attachmentUploadCounter = PerfCounter.GetCounter(PerfCounter.ATTACHMENT_UPLOAD_COUNT);
+
+			bytesToDownloadCounter = PerfCounter.GetCounter(PerfCounter.BYTES_TO_DOWNLOAD);
+			bytesToUploadCounter = PerfCounter.GetCounter(PerfCounter.BYTES_TO_UPLOAD);
 		}
 
 		public static UploadDownloadMonitor Instance
@@ -67,22 +72,64 @@ namespace Wammer.PerfMonitor
 		public void OnTaskEnqueued(object sender, Wammer.Station.TaskEventArgs arg)
 		{
 			if (arg.task is Wammer.Station.Timeline.ResourceDownloadTask)
+			{
+				var bytesToDownload = getDownloadSize(arg);
+
+				bytesToDownloadCounter.IncrementBy(bytesToDownload);
 				DownstreamNumCounter.Increment();
+			}
 			else if (arg.task is Wammer.Station.AttachmentUpload.UpstreamTask)
+			{
 				UpstreamNumCounter.Increment();
+
+				var uploadBytes = getUploadSize(arg);
+				bytesToUploadCounter.IncrementBy(uploadBytes);
+			}
 		}
 
 		public void OnTaskDequeued(object sender, Wammer.Station.TaskEventArgs arg)
 		{
 			if (arg.task is Wammer.Station.Timeline.ResourceDownloadTask)
+			{
+				var bytesDownload = getDownloadSize(arg);
+				bytesToDownloadCounter.IncrementBy(-bytesDownload);
 				DownstreamNumCounter.Decrement();
+			}
 			else if (arg.task is Wammer.Station.AttachmentUpload.UpstreamTask)
+			{
 				UpstreamNumCounter.Decrement();
+
+				var uploadBytes = getUploadSize(arg);
+				bytesToUploadCounter.IncrementBy(-uploadBytes);
+			}
 		}
 
 		public void OnDownstreamTaskInProgress(object sender, ProgressChangedEventArgs arg)
 		{
 			DownstreamRateCounter.IncrementBy(Convert.ToInt64(arg.UserState));
+		}
+
+		private static long getDownloadSize(Wammer.Station.TaskEventArgs arg)
+		{
+			var downloadTask = arg.task as Wammer.Station.Timeline.ResourceDownloadTask;
+
+			var bytesToDownload = 0L;
+			if (downloadTask.evtargs.imagemeta == ImageMeta.Origin || downloadTask.evtargs.imagemeta == ImageMeta.None)
+				bytesToDownload = downloadTask.evtargs.attachment.file_size;
+			else
+				bytesToDownload = downloadTask.evtargs.attachment.GetThumbnail(downloadTask.evtargs.imagemeta).file_size;
+			return bytesToDownload;
+		}
+
+		private long getUploadSize(Station.TaskEventArgs arg)
+		{
+			var task = (Wammer.Station.AttachmentUpload.UpstreamTask)arg.task;
+			var att = AttachmentCollection.Instance.FindOneById(task.object_id);
+
+			if (att == null || att.GetInfoByMeta(task.meta) == null)
+				return 0L;
+
+			return att.GetInfoByMeta(task.meta).file_size;
 		}
 	}
 }
