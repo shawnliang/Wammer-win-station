182ab7d53aeef47eb038f32cd977e1f292655105
Shawn Liang <shawn.liang@waveface.com>
2013-02-21 16:28:27 +0800

    connect service: apply zh-TW  UI string

--- Source/Waveface.Stream.WindowsClient/Src/Control/ServiceImportControl.designer.cs
+++ Source/Waveface.Stream.WindowsClient/Src/Control/ServiceImportControl.designer.cs
@@ -28,6 +28,7 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ServiceImportControl));
 			this.svcPanel = new System.Windows.Forms.FlowLayoutPanel();
 			this.label1 = new System.Windows.Forms.Label();
 			this.label2 = new System.Windows.Forms.Label();
@@ -35,46 +36,27 @@
 			// 
 			// svcPanel
 			// 
-			this.svcPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-			this.svcPanel.AutoScroll = true;
+			resources.ApplyResources(this.svcPanel, "svcPanel");
 			this.svcPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-			this.svcPanel.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
-			this.svcPanel.Location = new System.Drawing.Point(3, 45);
 			this.svcPanel.Name = "svcPanel";
-			this.svcPanel.Size = new System.Drawing.Size(547, 295);
-			this.svcPanel.TabIndex = 4;
-			this.svcPanel.WrapContents = false;
 			// 
 			// label1
 			// 
-			this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(5, 352);
+			resources.ApplyResources(this.label1, "label1");
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(244, 12);
-			this.label1.TabIndex = 3;
-			this.label1.Text = "You can change theses settings whenever you want.";
 			// 
 			// label2
 			// 
-			this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-			this.label2.Location = new System.Drawing.Point(3, 9);
+			resources.ApplyResources(this.label2, "label2");
 			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(547, 33);
-			this.label2.TabIndex = 2;
-			this.label2.Text = "A better way to fill your journal with memories from the past. Connect o all your" +
-    " favorite web services and let Stream generate real life events for you.";
 			// 
 			// ServiceImportControl
 			// 
+			resources.ApplyResources(this, "$this");
 			this.Controls.Add(this.svcPanel);
 			this.Controls.Add(this.label1);
 			this.Controls.Add(this.label2);
 			this.Name = "ServiceImportControl";
-			this.Size = new System.Drawing.Size(555, 369);
 			this.Load += new System.EventHandler(this.ServiceImportControl_Load);
 			this.ResumeLayout(false);
 			this.PerformLayout();
